1da1ddcc7ef7f59dce8e5c440743b3f69a10b205
Vitalii F <frankovitalii@gmail.com>
2018-08-02 19:01:16 -0400

    api_test_case: create abstract class ApiTestCase

---
+++ tests/Unit/Api/ApiTestCase.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit\Api;
+
+use Tests\TestCase;
+
+/**
+ * Class ApiTestCase
+ * @package Tests\Unit\Api
+ */
+abstract class ApiTestCase extends TestCase
+{
+    const GET = 'GET';
+    const POST = 'POST';
+    const PUT = 'PUT';
+    const PATCH = 'PUT';
+    const DELETE = 'DELETE';
+
+    abstract public function testGet();
+    abstract public function testPost();
+    abstract public function testPut();
+    abstract public function testDelete();
+
+    abstract protected function getApiClass() : string;
+    abstract protected function getEndpoint() : string;
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Check if response is json with a structure and status 200
+     * @param string $uri
+     * @param string $apiType
+     * @param array $jsonStructure
+     */
+    protected function isSuccessJsonResponse($uri, $apiType, $jsonStructure)
+    {
+        $this->json($apiType, $uri)
+            ->assertHeader('Content-Type', 'application/json')
+            ->assertStatus(200)
+            ->assertJsonStructure($jsonStructure);
+    }
+
+    /**
+     * Check if response return status 404
+     * @param string $uri
+     * @param string $apiType
+     */
+    protected function isNotExistingResponse($uri, $apiType)
+    {
+        $this->json($apiType, $uri)->assertStatus(404);
+    }
+}
