d90c79fd92b9aa61c3cfdda774260222b2a77a8c
Vitalii F <frankovitalii@gmail.com>
2018-08-02 18:43:09 -0400

    favourite_places: migration. add favourite_places table

---
+++ database/migrations/2018_08_02_222858_add_favourite_place_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFavouritePlaceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favourite_places', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('place_id');
+            
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->foreign('place_id')
+                ->references('id')
+                ->on('places');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favourite_places');
+    }
+}
