680152ed441441d1ee3a5fc0964e19226edb687d
k0sen <caladien12@gmail.com>
2018-08-02 23:43:59 +0300

    places_api_endpoints: create resource api & controller, tests

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Hedonist\Http\Controllers;
+
+use Hedonist\Entity\Place;
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Hedonist\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Place $place)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Hedonist\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Place $place)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Hedonist\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Place $place)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Hedonist\Place  $place
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Place $place)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,6 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
     });
-    // routes here
+
+    Route::resource('places', 'PlaceController');
 });
