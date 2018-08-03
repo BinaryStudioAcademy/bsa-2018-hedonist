ea9d2732347c4d6f35ec5430e1a654548bb92027
aLLeXUs <mrallexus@gmail.com>
2018-08-04 00:09:29 +0300

    Added like/dislike requests

---
+++ app/Actions/Like/DislikePlaceRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Hedonist\Actions\Like;
+
+use Hedonist\Actions\RequestInterface;
+
+class DislikePlaceRequest implements RequestInterface
+{
+
+}
---
+++ app/Actions/Like/LikePlaceRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Hedonist\Actions\Like;
+
+use Hedonist\Actions\RequestInterface;
+
+class LikePlaceRequest implements RequestInterface
+{
+
+}
