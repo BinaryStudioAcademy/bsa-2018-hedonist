c536db0641235e333af495f77d0ebde66e90ef04
ivanshamis <ivanshamis@gmail.com>
2018-08-03 15:26:11 +0300

    implement-place-category: remove Transformable

--- app/Entities/Place/PlaceCategory.php
+++ app/Entities/Place/PlaceCategory.php
@@ -3,13 +3,9 @@
 namespace Hedonist\Entities\Place;
 
 use Illuminate\Database\Eloquent\Model;
-use Prettus\Repository\Contracts\Transformable;
-use Prettus\Repository\Traits\TransformableTrait;
 
 class PlaceCategory extends Model implements Transformable
 {
-    use TransformableTrait;
-
     public $timestamps = false;
 
     protected $table = 'place_categories';
