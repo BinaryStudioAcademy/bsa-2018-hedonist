e84daad2864490eaa4ab10c675521635fcf965f1
Vitalii F <frankovitalii@gmail.com>
2018-08-03 06:06:15 -0400

    api_test_case: code review

--- tests/Feature/Api/ApiTestCase.php
+++ tests/Feature/Api/ApiTestCase.php
@@ -10,37 +10,7 @@ use Tests\TestCase;
  */
 abstract class ApiTestCase extends TestCase
 {
-    const GET = 'GET';
-    const POST = 'POST';
-    const PUT = 'PUT';
-    const PATCH = 'PUT';
-    const DELETE = 'DELETE';
-
-    abstract public function testGet();
-    abstract public function testPost();
-    abstract public function testPut();
-    abstract public function testDelete();
-
-    abstract protected function getApiClass() : string;
-    abstract protected function getEndpoint() : string;
-
-    protected function setUp()
-    {
-        parent::setUp();
-    }
-
-    protected function tearDown()
-    {
-        parent::tearDown();
-    }
-
-    /**
-     * Check if response is json with a structure and status 200
-     * @param string $uri
-     * @param string $apiType
-     * @param array $jsonStructure
-     */
-    protected function isSuccessJsonResponse($uri, $apiType, $jsonStructure)
+    protected function assertSuccessJsonResponse(string $uri, string $apiType, array $jsonStructure)
     {
         $this->json($apiType, $uri)
             ->assertHeader('Content-Type', 'application/json')
@@ -48,12 +18,7 @@ abstract class ApiTestCase extends TestCase
             ->assertJsonStructure($jsonStructure);
     }
 
-    /**
-     * Check if response return status 404
-     * @param string $uri
-     * @param string $apiType
-     */
-    protected function isNotExistingResponse($uri, $apiType)
+    protected function assertNotFoundJsonResponse(string $uri, string $apiType)
     {
         $this->json($apiType, $uri)->assertStatus(404);
     }
