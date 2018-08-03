95d2e024074ae41f649d7094823eb7cdc23e7279
aLLeXUs <mrallexus@gmail.com>
2018-08-04 00:10:07 +0300

    Added like/dislike actions

---
+++ app/Actions/Like/DislikePlaceAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hedonist\Actions\Like;
+
+use Hedonist\Actions\{ActionInterface, RequestInterface, ResponseInterface};
+
+class DislikePlaceAction implements ActionInterface
+{
+    public function execute(RequestInterface $request): ResponseInterface
+    {
+        // TODO: Implement execute() method.
+    }
+}
