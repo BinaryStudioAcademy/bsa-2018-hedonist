2d77ebe93cebb904130c66b0805de563fb9a3b14
Kolya Vantukh <vkolya0@gmail.com>
2018-08-03 15:53:10 +0300

    feature/review_model_migration: remove dockblocks

--- app/Entities/Review/ReviewPhoto.php
+++ app/Entities/Review/ReviewPhoto.php
@@ -7,27 +7,13 @@ use Hedonist\Entities\Review\Review;
 
 class ReviewPhoto extends Model
 {
-    /**
-     * Indicates if the model should be timestamped.
-     *
-     * @var bool
-     */
+     
     public $timestamps = false;
-   /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
+    
     protected $table = 'review_photos';
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+     
     protected $fillable = ['img_url','description','review_id'];
-    /**
-     * Get photo's review
-     */
+     
     public function review()
     {
         return $this->belongsTo(Review::class);
