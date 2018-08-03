5f81d083e90008afcc547731055851b1b482b55e
ivanshamis <ivanshamis@gmail.com>
2018-08-03 16:09:59 +0300

    implement-place-category: fix field name

--- database/migrations/2018_08_02_225416_create_place_categories_table.php
+++ database/migrations/2018_08_02_225416_create_place_categories_table.php
@@ -18,7 +18,7 @@ class CreatePlaceCategoriesTable extends Migration
     {
         Schema::create('place_categories', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 45);
+            $table->string('name');
         });
     }
 
