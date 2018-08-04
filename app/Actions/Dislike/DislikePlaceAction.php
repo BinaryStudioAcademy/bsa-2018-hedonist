fa4de1f9533db62c84369795c1bfedf85744785e
aLLeXUs <mrallexus@gmail.com>
2018-08-04 23:53:57 +0300

    Implemented actions

--- app/Actions/Dislike/DislikePlaceAction.php
+++ app/Actions/Dislike/DislikePlaceAction.php
@@ -2,19 +2,42 @@
 
 namespace Hedonist\Actions\Dislike;
 
+use Hedonist\Repositories\Like\LikeRepository;
 use Hedonist\Repositories\Dislike\DislikeRepository;
+use Illuminate\Support\Facades\Auth;
 
 class DislikePlaceAction
 {
+    private $likeRepository;
     private $dislikeRepository;
 
-    public function __construct(DislikeRepository $dislikeRepository)
+    public function __construct(LikeRepository $likeRepository, DislikeRepository $dislikeRepository)
     {
+        $this->likeRepository = $likeRepository;
         $this->dislikeRepository = $dislikeRepository;
     }
 
     public function execute(DislikePlaceRequest $request): DislikePlaceResponse
     {
-        // TODO: Implement execute() method.
+        $like = $this->likeRepository->model()->where([
+            ['likeable_id', $request->getPlaceId()],
+            ['likeable_type', 'places']
+        ])->get();
+        $dislike = $this->dislikeRepository->model()->where([
+            ['dislikeable_id', $request->getPlaceId()],
+            ['dislikeable_type', 'places']
+        ])->get();
+        if ($like) {
+            $this->likeRepository->deleteById($like->id);
+        }
+        if (empty($dislike)) {
+            $dislike = new Dislike([
+                'dislikeable_id' => $request->getPlaceId(),
+                'dislikeable_type' => 'places',
+                'user_id' => Auth::id()
+            ]);
+            $this->dislikeRepository->save($dislike);
+        }
+        return new DislikePlaceResponse();
     }
 }
--- app/Actions/Like/LikePlaceAction.php
+++ app/Actions/Like/LikePlaceAction.php
@@ -3,18 +3,41 @@
 namespace Hedonist\Actions\Like;
 
 use Hedonist\Repositories\Like\LikeRepository;
+use Hedonist\Repositories\Dislike\DislikeRepository;
+use Illuminate\Support\Facades\Auth;
 
 class LikePlaceAction
 {
     private $likeRepository;
+    private $dislikeRepository;
 
-    public function __construct(LikeRepository $likeRepository)
+    public function __construct(LikeRepository $likeRepository, DislikeRepository $dislikeRepository)
     {
         $this->likeRepository = $likeRepository;
+        $this->dislikeRepository = $dislikeRepository;
     }
 
     public function execute(LikePlaceRequest $request): LikePlaceResponse
     {
-        // TODO: Implement execute() method.
+        $like = $this->likeRepository->model()->where([
+            ['likeable_id', $request->getPlaceId()],
+            ['likeable_type', 'places']
+        ])->get();
+        $dislike = $this->dislikeRepository->model()->where([
+            ['dislikeable_id', $request->getPlaceId()],
+            ['dislikeable_type', 'places']
+        ])->get();
+        if ($dislike) {
+            $this->dislikeRepository->deleteById($dislike->id);
+        }
+        if (empty($like)) {
+            $like = new Like([
+                'likeable_id' => $request->getPlaceId(),
+                'likeable_type' => 'places',
+                'user_id' => Auth::id()
+            ]);
+            $this->likeRepository->save($like);
+        }
+        return new LikePlaceResponse();
     }
 }
