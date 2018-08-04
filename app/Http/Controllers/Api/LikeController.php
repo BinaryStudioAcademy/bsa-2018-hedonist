1a901d926be8fca009a77c5a31f8b809e0dbdb14
aLLeXUs <mrallexus@gmail.com>
2018-08-04 03:27:03 +0300

    Implemented controller

--- app/Http/Controllers/Api/LikeController.php
+++ app/Http/Controllers/Api/LikeController.php
@@ -2,16 +2,38 @@
 
 namespace Hedonist\Http\Controllers\Api;
 
+use Hedonist\Actions\Like\{DislikePlaceAction, DislikePlaceRequest, LikePlaceAction, LikePlaceRequest};
 
 class LikeController extends ApiController
 {
-    public function likePlace()
+    private $likePlaceAction;
+    private $dislikePlaceAction;
+
+    public function __construct(LikePlaceAction $likePlaceAction, DislikePlaceAction $dislikePlaceAction)
     {
-        return successResponse('ok', 200);
+        $this->likePlaceAction = $likePlaceAction;
+        $this->dislikePlaceAction = $dislikePlaceAction;
     }
 
-    public function dislikePlace()
+    public function likePlace(int $id)
     {
-        return successResponse('ok', 200);
+        try {
+            $request = new LikePlaceRequest($id);
+            $response = $this->likePlaceAction->execute($request);
+        } catch (\Exception $exception) {
+            return $this->errorResponse($exception->getMessage(), 400);
+        }
+        return $this->successResponse('ok', 200);
+    }
+
+    public function dislikePlace(int $id)
+    {
+        try {
+            $request = new DislikePlaceRequest($id);
+            $response = $this->dislikePlaceAction->execute($request);
+        } catch (\Exception $exception) {
+            return $this->errorResponse($exception->getMessage(), 400);
+        }
+        return $this->successResponse('ok', 200);
     }
 }
