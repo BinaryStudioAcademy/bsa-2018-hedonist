85aa512125dc48146f4b702d941ba5f119c19ef0
fadykstas <stas.fadeev@gmail.com>
2018-08-04 21:51:26 +0300

    Update PlacesFeaturesFactory.php

--- database/factories/PlacesFeaturesFactory.php
+++ database/factories/PlacesFeaturesFactory.php
@@ -15,12 +15,20 @@ use Hedonist\Entities\Place\PlaceFeature;
 */
 $factory->define(PlaceFeature::class, function (Faker $faker) {
  
-    $array = ['cafe',
-            'restaurant',
-            'lunch',
-            'bar',
-            'coffee',
-            'pizzeria'];
+    $array = [
+            'wi-fi',
+            'music',
+            'credit cards',
+            'hookah',
+            'outdoor seating',
+            'wheelchair accessible',
+            'reservations',
+            'parking',
+            'restroom',
+            'take-out',
+            'delivery',
+            'live music'
+    ];
 
     return [
         'name' => $faker->unique()->randomElement($array)
