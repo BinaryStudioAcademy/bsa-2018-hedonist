937be67b1fdf834b3cc2490d4f4db6f142b617f2
aLLeXUs <mrallexus@gmail.com>
2018-08-04 03:26:50 +0300

    Added like repository to actions

--- app/Actions/Like/DislikePlaceAction.php
+++ app/Actions/Like/DislikePlaceAction.php
@@ -3,9 +3,17 @@
 namespace Hedonist\Actions\Like;
 
 use Hedonist\Actions\{ActionInterface, RequestInterface, ResponseInterface};
+use Hedonist\Repositories\Like\LikeRepository;
 
 class DislikePlaceAction implements ActionInterface
 {
+    private $likeRepository;
+
+    public function __construct(LikeRepository $likeRepository)
+    {
+        $this->likeRepository = $likeRepository;
+    }
+
     public function execute(RequestInterface $request): ResponseInterface
     {
         // TODO: Implement execute() method.
