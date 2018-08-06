28ecf6097f71a612ecc50d1913b49e2fb37cc635
ivanshamis <ivanshamis@gmail.com>
2018-08-06 07:22:46 +0300

    implement-ratings: fix migration

--- app/Entities/Place/Rating.php
+++ app/Entities/Place/Rating.php
@@ -3,17 +3,12 @@
 namespace Hedonist\Entities\Place;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rating extends Model
 {
-    use SoftDeletes;
-
     public $timestamps = false;
 
     protected $table = 'ratings';
 
     protected $fillable = ['rating'];
-
-    protected $dates = ['deleted_at'];
 }
