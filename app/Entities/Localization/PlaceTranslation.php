fc83c46f8d09f126e0210d4f66b749936f5f5516
MihailTS <tseluiko.m@gmail.com>
2018-08-06 05:22:48 +0300

    places: add localization entities

---
+++ app/Entities/Localization/PlaceTranslation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hedonist\Entities\Localization;
+
+use Hedonist\Entities\Place\Place;
+use Illuminate\Database\Eloquent\Model;
+
+class PlaceTranslation extends Model
+{
+    protected $table = "places_tr";
+
+    protected $fillable = [
+        "place_name",
+        "place_description",
+        "place_id",
+        "language_id"
+    ];
+
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
+
+    public function language()
+    {
+        return $this->belongsTo(Language::class);
+    }
+}
