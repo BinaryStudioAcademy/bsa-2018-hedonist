d02b29625d65da6dd6585127c92cc64addfe864e
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-08-06 12:02:30 +0300

    Correct DeleteReview Action.

--- app/Actions/Review/DeleteReviewAction.php
+++ app/Actions/Review/DeleteReviewAction.php
@@ -2,19 +2,18 @@
 
 namespace Hedonist\Actions\Review;
 
-//use Hedonist\Repositories\Review\ReviewRepository;
+use Hedonist\Repositories\Review\ReviewRepository;
 
 class DeleteReviewAction
 {
     private $reviewRepository;
 
-    /* ReviewRepository does NOT created at the moment */
     public function __construct(ReviewRepository $repository)
     {
         $this->reviewRepository = $repository;
     }
 
-    public function execute(DeleteReviewRequest $request)
+    public function execute(DeleteReviewRequest $request): void
     {
         $this->reviewRepository->deleteById($request->getReviewId());
     }
