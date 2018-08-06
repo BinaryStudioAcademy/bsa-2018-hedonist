d1fadff33120c4cd71c2b7f535116967912ad0fe
Alexander Kharchenko <sandrokharchenko@gmail.com>
2018-08-06 16:07:20 +0300

    place: add place categories tags factory and seeder

---
+++ database/seeds/PlaceCategoriesTagsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Hedonist\Entities\Place\PlaceCategoryTag;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class PlaceCategoriesTagsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('place_categories_tags')->truncate();
+
+        factory(PlaceCategoryTag::class, 30)->create();
+    }
+}
