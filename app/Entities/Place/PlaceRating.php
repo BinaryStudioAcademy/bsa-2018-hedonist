170fc28b2183675132454e96573e67aa95b568f5
ivanshamis <ivanshamis@gmail.com>
2018-08-06 11:03:19 +0300

    implement-ratings: fix model

--- app/Entities/Place/PlaceRating.php
+++ app/Entities/Place/PlaceRating.php
@@ -10,5 +10,15 @@ class PlaceRating extends Model
 
     protected $table = 'place_rating';
 
-    protected $fillable = ['rating'];
+    protected $fillable = ['user_id', 'place_id', 'rating'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+   
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
 }
