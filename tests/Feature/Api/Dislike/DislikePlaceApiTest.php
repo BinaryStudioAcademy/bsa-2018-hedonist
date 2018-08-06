025f6c247f4724d14c4dc97edecfa2a0ddc28fbd
aLLeXUs <mrallexus@gmail.com>
2018-08-07 01:19:34 +0300

    Add auth to api test

--- tests/Feature/Api/Dislike/DislikePlaceApiTest.php
+++ tests/Feature/Api/Dislike/DislikePlaceApiTest.php
@@ -2,13 +2,18 @@
 
 namespace Tests\Feature\Api\Dislike;
 
+use Hedonist\Entities\User\User;
 use Tests\Feature\Api\ApiTestCase;
 
 class DislikePlaceApiTest extends ApiTestCase
 {
     public function testDislikePlaceNotFound()
     {
-        $response = $this->post('/api/v1/places/99999/dislike');
+        $user = factory(User::class)->create();
+        $token = \JWTAuth::fromUser($user);
+
+        $response = $this->post('/api/v1/places/99999/dislike', [], ['Authorization' => 'Bearer ' . $token]);
+        
         $response->assertHeader('Content-Type', 'application/json')
             ->assertNotFound()
             ->assertJsonStructure([
--- tests/Feature/Api/Like/LikePlaceApiTest.php
+++ tests/Feature/Api/Like/LikePlaceApiTest.php
@@ -2,13 +2,31 @@
 
 namespace Tests\Feature\Api\Like;
 
+use Hedonist\Entities\User\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Feature\Api\ApiTestCase;
 
 class LikePlaceApiTest extends ApiTestCase
 {
+    use RefreshDatabase;
+
+    private function login(User $user): array
+    {
+        $response = $this->json('POST', '/api/v1/auth/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ]);
+
+        return $response->json('data');
+    }
+
     public function testLikePlaceNotFound()
     {
-        $response = $this->post('/api/v1/places/99999/like');
+        $user = factory(User::class)->create();
+        $token = \JWTAuth::fromUser($user);
+
+        $response = $this->post('/api/v1/places/99999/like', [], ['Authorization' => 'Bearer ' . $token]);
+
         $response->assertHeader('Content-Type', 'application/json')
             ->assertNotFound()
             ->assertJsonStructure([
