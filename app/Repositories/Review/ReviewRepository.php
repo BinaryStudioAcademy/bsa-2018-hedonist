2a4a61ebb6fa0986509dc12b36120d23500d0d11
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-08-06 11:26:54 +0300

    ReviewRepository added.

---
+++ app/Repositories/Review/ReviewRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Hedonist\Repositories\Review;
+
+use Hedonist\Entities\Review\Review;
+use Illuminate\Database\Eloquent\Collection;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class ReviewRepository extends BaseRepository implements ReviewRepositoryInterface
+{
+    public function findAll(): Collection
+    {
+        return Review::all();
+    }
+
+    public function getById(int $id): ?Review
+    {
+        return Review::find($id);
+    }
+
+    public function save(Review $review): Review
+    {
+        $review->save();
+        return $review;
+    }
+
+    public function deleteById(int $id): void
+    {
+        if ($review = Review::find($id)) {
+            $review->delete();
+        }
+    }
+
+    public function model()
+    {
+        return Review::class;
+    }
+}
