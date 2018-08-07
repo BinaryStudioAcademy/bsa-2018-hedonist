ecb6b12733b7a4f21f2f4024b1ec3145c814d15b
Лесогорский Кирилл <lesogor.kirill@gmail.com>
2018-08-07 16:41:27 +0300

    wip

---
+++ frontend/src/components/PlacesList/PlaceListComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <transition>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="img-fluid">
+                        <img :src="place.image">
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class = "row">
+                        <div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+    >
+</template>
+
+<script>
+    export default {
+        name: "PlaceListComponent",
+        props:
+            {
+                place:
+                    {
+                        required: true,
+                        type: Object,
+                    }
+            },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
