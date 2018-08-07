7ac6e4091e2ccced7c17aa9f476258ab796f823b
Stan <stas.fadeev@gmail.com>
2018-08-07 13:24:46 +0300

    feature/jwt_auth_added_to_api_test_case

--- tests/Feature/Api/ApiTestCase.php
+++ tests/Feature/Api/ApiTestCase.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature\Api;
 
+use Illuminate\Foundation\Testing\TestResponse;
 use Tests\TestCase;
+use Hedonist\Entities\User\User;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
 
 /**
  * Class ApiTestCase
@@ -10,6 +13,28 @@ use Tests\TestCase;
  */
 abstract class ApiTestCase extends TestCase
 {
+    protected $authenticatedUser;
+
+    public function actingAs(UserContract $user, $driver = null) : self
+    {
+        $this->authenticatedUser = $user;
+        return $this;
+    }
+
+    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null) : TestResponse
+    {
+        if ($this->authenticatedUser) {
+            $server['HTTP_AUTHORIZATION'] = 'Bearer ' . auth()->login($this->authenticatedUser);
+        }
+        $server['HTTP_ACCEPT'] = 'application/json';
+        return parent::call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
+
+    protected function actingAsJwtToken() : self
+    {
+        return $this->actingAs(factory(User::class)->create());
+    }
+
     protected function assertSuccessJsonResponse(string $uri, string $apiType, array $jsonStructure)
     {
         $this->json($apiType, $uri)
