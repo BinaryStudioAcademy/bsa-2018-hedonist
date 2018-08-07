328955e759e80fe5b7f86ef82c8e4c224fdacefa
k0sen <caladien12@gmail.com>
2018-08-07 20:47:12 +0300

    feature/place_location_entity: implement location entity

--- app/Entities/Place/Place.php
+++ app/Entities/Place/Place.php
@@ -68,4 +68,10 @@ class Place extends Model
     {
         return $this->belongsToMany(PlaceFeature::class, 'places_places_features');
     }
+
+    public function setLocation(Location $location): void
+    {
+        $this->latitude = $location->getLatitude();
+        $this->longitude = $location->getLongitude();
+    }
 }
