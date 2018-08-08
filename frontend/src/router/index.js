d7712a5b8affa1472fc9d282b63436818d719140
Лесогорский Кирилл <lesogor.kirill@gmail.com>
2018-08-08 22:32:52 +0300

    Finished component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import PlacesList from  '@/components/PlacesList/PlacesList'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            redirect:'/placesList',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/placesList',
+            component: PlacesList
+        }
+    ]
 })
