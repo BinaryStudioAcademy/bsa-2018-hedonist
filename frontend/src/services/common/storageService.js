4d71e364ea399a82f7c3b2c12a4f17351cda46ac
Vitalii F <frankovitalii@gmail.com>
2018-08-08 06:03:55 -0400

    http_auth_frontend: add get, set to storageService

--- frontend/src/services/common/storageService.js
+++ frontend/src/services/common/storageService.js
@@ -1,11 +1,16 @@
-
 export const storageService = {
     keyName: 'user_token',
+    get(key) {
+        return localStorage.getItem(key);
+    },
+    set(key, value) {
+        return localStorage.setItem(key, value);
+    },
     getToken() {
-        return localStorage.getItem(this.keyName);
+        return this.get(this.keyName);
     },
     setToken(token) {
-        return localStorage.setItem(this.keyName, token);
+        return this.set(this.keyName, token);
     },
     removeToken() {
         return localStorage.removeItem(this.keyName);
