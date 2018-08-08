3132c7cca98498cb9ec19c0ab78f923aea203a95
ivanshamis <ivanshamis@gmail.com>
2018-08-08 20:28:27 +0300

    frontend-add-auth-middleware: add router.beforeEach

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 import HelloWorld from '@/components/HelloWorld'
 import ProfilePage from '@/pages/ProfilePage';
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes: [
         {
             path: '/',
             name: 'HelloWorld',
-            component: HelloWorld
+            component: HelloWorld,
         },
         {
             path: '/profile',
             name: 'ProfilePage',
-            component: ProfilePage
+            component: ProfilePage,
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: LoginPage,
+            meta: { notRequiresAuth: true }
         }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.notRequiresAuth)) {
+        next()
+    } else {
+        if (!store.getters['auth/isLoggedIn']) {
+            next({
+                path: '/login',
+                query: { redirect: to.fullPath }
+              })
+        } else {
+            next()
+        }
+    }
+})
+
+export default router
