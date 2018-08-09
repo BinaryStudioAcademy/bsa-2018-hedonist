00c88064b077d785c9c0d35952926848084973c1
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-08-09 10:59:51 +0300

    locationService: add stub retrieving location data

--- frontend/src/services/location/locationService.js
+++ frontend/src/services/location/locationService.js
@@ -2,14 +2,6 @@ import Vue from 'vue';
 
 export default {
     getUserLocationData(){
-        Vue.http.get('userData')
-            .then(
-                (response) => {
-                    success(response.data)
-                },
-                (response) => {
-                    error(response)
-                }
-            )
+
     }
 }
