6e12da7505968fa7d99e464f495a7f831b4b83b2
Vitalii F <frankovitalii@gmail.com>
2018-08-09 17:15:16 -0400

    history_page: add pages and components

---
+++ frontend/src/components/Places/VisitedPlacesComponent.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="container place-item">
+
+        <article>
+            <div class="entry-media">
+                <img class="image" :src="visitedPlace.place_photo.url"/>
+            </div>
+            <div class="item-description">
+                <div class="rating-wrapper">
+                    <div class="rating">
+                        {{visitedPlace.ratings.rating}}
+                    </div>
+                </div>
+                <h2 class="title">{{ visitedPlace.places_tr.place_name }}</h2>
+                <p>{{ visitedPlace.address }}, {{ visitedPlace.cities.name }}</p>
+                <p>{{ visitedPlace.categories.name }} - Подсказок и отзывов: </p>
+
+                <button class="saved" disabled><i class="fa fa-star"></i>Сохранено</button>
+            </div>
+        </article>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .container {
+        display: flex;
+        flex-wrap: nowrap;
+    }
+
+    article {
+        margin: 0.5rem;
+        text-align: left;
+    }
+
+    .image {
+        margin: 0 auto;
+        max-width: 400px;
+    }
+
+    .item-description {
+        .rating-wrapper {
+            float: right;
+        }
+
+        p {
+            padding-left: 0.5rem;
+        }
+
+        .saved {
+            margin-top: 2rem;
+        }
+    }
+
+    .title {
+        padding-top: 0.4rem;
+        padding-left: 0.5rem;
+    }
+
+    .rating-wrapper {
+        margin-top: 1.5rem;
+    }
+    .rating {
+        width: 48px;
+        height: 48px;
+        background: #00E676;
+        border-radius: 7px;
+        margin: auto;
+        line-height: 48px;
+        font-size: 1.5rem;
+        color: #FFF;
+        text-align: center;
+    }
+</style>
+
+<script>
+    export default {
+        name: "VisitedPlacesComponent",
+        props: {
+            visitedPlace: {
+                required: true,
+                type: Object,
+            }
+        }
+    }
+</script>
---
+++ frontend/src/pages/VisitedPlacesPage.vue
@@ -0,0 +1,110 @@
+<template>
+    <section class="container">
+        <ul>
+            <li v-for="(visitedPlace,index) in visitedPlaces" :key="visitedPlace.id">
+                <VisitedPlacesComponent :visitedPlace="visitedPlace" :timer="50 * (index)"/>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+    import VisitedPlacesComponent from '@/components/Places/VisitedPlacesComponent';
+
+    export default {
+        name: "VisitedPlacesPage",
+        data() {
+            return {
+                visitedPlaces: [
+                    {
+                        id: 1,
+                        ratings: {
+                            rating: 10
+                        },
+                        address: "ул. Кошки, 43",
+                        longitude: null,
+                        latitude: null,
+                        zip: null,
+                        creator_id: null,
+                        categories: {
+                            id: 10,
+                            name: 'Парк'
+                        },
+                        place_photo: {
+                            url: "https://i.ytimg.com/vi/3lqlc-ooJpE/hqdefault.jpg",
+                            description: "Чебуречек",
+                            posted_at: '2018-08-08'
+                        },
+                        reviews: {
+
+                        },
+                        places_tr: {
+                            id: 2,
+                            place_name: "Чебуречек",
+                            place_description: "Чебуречек очень вкусный",
+                            language_id: 2
+                        },
+                        cities: {
+                            id: 10,
+                            name: "Львов"
+                        }
+                    },
+                    {
+                        id: 2,
+                        rating: 10,
+                        address: "вул. Кошки, 43",
+                        longitude: null,
+                        latitude: null,
+                        zip: null,
+                        creator_id: null,
+                        category_id: 1,
+                        place_photo: {
+                            url: "https://media4.designs.net/images/hero_illustration_left.png",
+                            description: "Чебуречек",
+                            posted_at: '2018-08-08'
+                        },
+                        reviews: {
+
+                        },
+                        places_tr: {
+                            id: 3,
+                            place_name: "Буржуй",
+                            place_description: "Чё?",
+                            language_id: 2
+                        }
+                    },
+                    {
+                        id: 3,
+                        rating: 10,
+                        address: "вул. Кошки, 43",
+                        longitude: null,
+                        latitude: null,
+                        zip: null,
+                        creator_id: null,
+                        category_id: 1,
+                        place_photo: {
+                            url: "https://media4.designs.net/images/hero_illustration_left.png",
+                            description: "Чебуречек",
+                            posted_at: '2018-08-08'
+                        },
+                        reviews: {
+
+                        },
+                        places_tr: {
+                            id: 3,
+                            place_name: "Буржуй",
+                            place_description: "Чё?",
+                            language_id: 2
+                        }
+                    }
+                ]
+            }
+        },
+        components: {VisitedPlacesComponent},
+    }
+</script>
+
