df6ad65519a46dde0ca7ea7bc6394c38e6c91e4e
Лесогорский Кирилл <lesogor.kirill@gmail.com>
2018-08-09 09:48:37 +0300

    Fixed permissions issue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import ProfilePage from '@/pages/ProfilePage';
+import PlacesList from  '@/components/PlacesList/PlacesList'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
             path: '/profile',
             name: 'ProfilePage',
             component: ProfilePage
+        },
+        {
+            path: '/places/list',
+            name: 'PlacesList',
+            component: PlacesList
         }
     ]
 })
