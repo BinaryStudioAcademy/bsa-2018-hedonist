e08de8c1dc014676c1650f61dc2ea8febde22ef8
MihailTS <tseluiko.m@gmail.com>
2018-08-09 10:44:58 +0300

    user_info_api: add tests

---
+++ tests/Feature/Api/UserInfo/UserInfoApiTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Tests\Feature\UserInfo;
+
+use Hedonist\Entities\User\User;
+use Hedonist\Entities\User\UserInfo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\JwtTestCase;
+
+class UserInfoApiTest extends JwtTestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->actingAsJwtToken();
+    }
+
+    public function test_get_user_info()
+    {
+        $userInfo = factory(UserInfo::class)->create();
+
+        $response = $this->json('GET', "/api/v1/users/$userInfo->user_id/info");
+        $response->assertHeader('Content-Type', 'application/json');
+        $response->assertStatus(200);
+        $data = json_decode($response->getContent(), true);
+
+        $this->assertEquals($userInfo->user_id, $data['data']['user_id']);
+        $this->assertEquals($userInfo->first_name, $data['data']['first_name']);
+        $this->assertEquals($userInfo->last_name, $data['data']['last_name']);
+        $this->assertEquals($userInfo->date_of_birth->format('Y/m/d'), $data['data']['date_of_birth']);
+        $this->assertEquals($userInfo->phone_number, $data['data']['phone_number']);
+        $this->assertEquals($userInfo->avatar_url, $data['data']['avatar_url']);
+        $this->assertEquals($userInfo->facebook_url, $data['data']['facebook_url']);
+        $this->assertEquals($userInfo->instagram_url, $data['data']['instagram_url']);
+        $this->assertEquals($userInfo->twitter_url, $data['data']['twitter_url']);
+    }
+
+    public function test_update_user_info()
+    {
+        $userInfo = factory(UserInfo::class)->create();
+        $data = [
+            'first_name' => "test_first_name",
+            'date_of_birth' => '1970/04/12',
+            'phone_number' => "380123456789"
+        ];
+
+        $response = $this->json('PUT', "/api/v1/users/$userInfo->user_id/info", $data);
+        $response->assertStatus(200);
+        $result = json_decode($response->getContent(), true);
+
+        $this->assertEquals($result['data']['user_id'], $userInfo->user_id);
+        $this->assertEquals($result['data']['first_name'], $data['first_name']);
+        $this->assertEquals($result['data']['last_name'], $userInfo->last_name);
+        $this->assertEquals($result['data']['date_of_birth'], $data['date_of_birth']);
+        $this->assertEquals($result['data']['phone_number'], $data['phone_number']);
+        $this->assertEquals($result['data']['avatar_url'], $userInfo->avatar_url);
+        $this->assertEquals($result['data']['facebook_url'], $userInfo->facebook_url);
+        $this->assertEquals($result['data']['instagram_url'], $userInfo->instagram_url);
+        $this->assertEquals($result['data']['twitter_url'], $userInfo->twitter_url);
+    }
+
+
+    public function test_create_user_info()
+    {
+        $user = factory(User::class)->create();
+
+        $data = [
+            'user_id' => $user->id,
+            'first_name' => "test_first_name",
+            'last_name' => "test_last_name",
+            'date_of_birth' => '1970/04/12',
+            'phone_number' => "380123456789",
+            'avatar_url' => "http://placehold.it/50/2",
+            'facebook_url' => "https://www.facebook.com/profile.php?id=123456789012345",
+            'instagram_url' => "https://www.instagram.com/12345/"
+        ];
+        $response = $this->json('PUT', "/api/v1/users/$user->id/info", $data);
+        $response->assertStatus(200);
+
+        $result = json_decode($response->getContent(), true);
+
+        $this->assertEquals($result['data']['user_id'], $user->id);
+        $this->assertEquals($result['data']['first_name'], $data['first_name']);
+        $this->assertEquals($result['data']['last_name'], $data['last_name']);
+        $this->assertEquals($result['data']['date_of_birth'], $data['date_of_birth']);
+        $this->assertEquals($result['data']['phone_number'], $data['phone_number']);
+        $this->assertEquals($result['data']['avatar_url'], $data['avatar_url']);
+        $this->assertEquals($result['data']['facebook_url'], $data['facebook_url']);
+        $this->assertEquals($result['data']['instagram_url'], $data['instagram_url']);
+        $this->assertEquals($result['data']['twitter_url'], "");
+
+        $this->assertDatabaseHas('user_info', $data);
+    }
+
+    public function test_create_user_info_without_required_field()
+    {
+        $user = factory(User::class)->create();
+
+        $data = [
+            'user_id' => $user->id,
+            'first_name' => "test_first_name",
+            'date_of_birth' => '1970/04/12',
+            'phone_number' => "380123456789",
+            'avatar_url' => "http://placehold.it/50/2",
+            'facebook_url' => "https://www.facebook.com/profile.php?id=123456789012345",
+            'instagram_url' => "https://www.instagram.com/12345/",
+            'twitter_url' => "https://twitter.com/12345"
+        ];
+
+        $response = $this->json('PUT', "/api/v1/users/$user->id/info", $data);
+        $response->assertStatus(400);
+    }
+
+    public function test_get_user_list_not_found()
+    {
+        $this->json('GET', "/api/v1/user/9999/info")->assertStatus(404);
+    }
+}
