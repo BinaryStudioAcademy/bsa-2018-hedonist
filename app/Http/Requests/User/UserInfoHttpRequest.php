12aed3bd3db86a028ca595b6ca16ed1ad2194ba0
MihailTS <tseluiko.m@gmail.com>
2018-08-09 10:44:48 +0300

    user_info_api: add UserInfoController and request

---
+++ app/Http/Requests/User/UserInfoHttpRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Hedonist\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserInfoHttpRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            "first_name" => 'max:255',
+            "last_name" => 'max:255',
+            "date_of_birth" => 'date_format:Y/m/d',
+            "phone_number" => 'max:25',
+            "avatar_url" => 'url',
+            "facebook_url" => 'url',
+            "instagram_url" => 'url',
+            "twitter_url" => 'url',
+        ];
+    }
+}
