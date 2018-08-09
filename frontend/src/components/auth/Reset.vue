64b3e1f7c0a9077b74c728eb6709790e8e5d810f
Alexander Kharchenko <sandrokharchenko@gmail.com>
2018-08-09 20:14:33 +0300

    auth: add logic to reset password page

--- frontend/src/components/auth/Reset.vue
+++ frontend/src/components/auth/Reset.vue
@@ -1,23 +1,43 @@
 <template>
   <Container title="Reset your password">
     <Form>
-      <b-field label="Current password">
-        <b-input type="password"
-          v-model="user.currentPassword"
-          placeholder="Curent password"
-          password-reveal>
-        </b-input>
-      </b-field>
+      <b-field
+        label="New password"
+        :type="input.password.type">
 
-      <b-field label="New password">
-        <b-input type="password"
-          v-model="user.newPassword"
+        <b-input 
+          type="password"
+          v-model="user.password"
           placeholder="New password"
-          password-reveal>
+          password-reveal
+          @blur="onBlur('password')"
+          @focus="onFocus('password')"
+          autofocus>
         </b-input>
       </b-field>
 
-      <button type="button" class="button is-primary is-rounded is-hovered button-wide">Reset</button>
+      <b-field
+        label="Confirm new password"
+        :type="input.passwordConfirm.type">
+
+        <b-input
+          type="password"
+          v-model="user.passwordConfirm"
+          placeholder="New password"
+          password-reveal
+          @blur="onBlur('passwordConfirm')"
+          @focus="onFocus('passwordConfirm')"
+          @keyup.enter="onReset">
+        </b-input>
+      </b-field>
+
+      <button
+        type="button"
+        class="button is-primary is-rounded is-hovered button-wide"
+        @click="onReset"
+        @keyup.enter="onReset">
+          Reset
+      </button>
     </Form>
 
     <link rel="stylesheet" href="//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css">
@@ -25,6 +45,8 @@
 </template>
 
 <script>
+import { required, email, minLength } from 'vuelidate/lib/validators'
+import { mapGetters, mapActions } from 'vuex';
 import Container from './Container'
 import Form from './Form'
 
@@ -37,8 +59,82 @@ export default {
   data: function () {
     return {
       user: {
-        currentPassword: '',
-        newPassword: ''
+        password: '',
+        passwordConfirm: ''
+      },
+
+      input: {
+        password: {
+          type: ''
+        },
+        passwordConfirm: {
+          type: ''
+        }
+      }
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+      'getAuthenticatedUser',
+      'getToken'
+    ])
+  },
+
+  methods: {
+    ...mapActions(['resetPassword']),
+
+    onReset () {
+      if (!this.$v.user.$invalid) {
+        this.resetPassword({
+          email: this.getAuthenticatedUser.email,
+          password: this.user.password,
+          passwordConfirmation: this.user.passwordConfirm,
+          token: this.getToken
+        })
+
+        this.refreshInput()
+      }
+    },
+
+    onBlur (el) {
+      if (this.$v.user[el].$invalid) {
+        this.input[el].type = 'is-danger'
+      } else {
+        this.input[el].type = 'is-success'
+      }
+    },
+
+    onFocus (el) {
+      this.input[el].type = ''
+    },
+
+    refreshInput () {
+      this.user = {
+        password: '',
+        passwordConfirm: ''
+      },
+
+      this.input = {
+        password: {
+          type: ''
+        },
+        passwordConfirm: {
+          type: ''
+        }
+      }
+    }
+  }, 
+
+  validations: {
+    user: {
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      passwordConfirm: {
+        required,
+        minLength: minLength(6)
       }
     }
   }
