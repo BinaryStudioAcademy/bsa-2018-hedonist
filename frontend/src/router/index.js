b87d7ce7d72c8bc1e8a3f38ef2b1b8c65fad9a87
ivanshamis <ivanshamis@gmail.com>
2018-08-09 16:13:35 +0300

    forntend-add-auth-middleware: import RouterMiddleware

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,34 +1,44 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import RouterMiddleware from 'vue-router-middleware';
 import HelloWorld from '@/components/HelloWorld';
 import ProfilePage from '@/pages/ProfilePage';
-import PlacesList from  '@/components/PlacesList/PlacesList'
+import PlacesList from  '@/components/PlacesList/PlacesList';
+import store from '../store/index';
 
 Vue.use(Router);
+
+const middleware = handler => (
+    routes => routes.map(route => Object.assign({}, route, { beforeEnter: handler }))
+);
 
 export default new Router({
     mode: 'history',
     base: '/',
     scrollBehavior: () => ({y: 0}),
     routes: [
-        {
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld,
-        },
-        {
-            path: '/profile',
-            name: 'ProfilePage',
-            component: ProfilePage,
-        },
-        {
-            path: '/places/list',
-            name: 'PlacesList',
-            component: PlacesList
-        },
-        {
-            path: '/login',
-            name: 'Login',
-        }
+        ...middleware(auth(store))([
+            {
+                path: '/',
+                name: 'HelloWorld',
+                component: HelloWorld,
+            },
+            {
+                path: '/profile',
+                name: 'ProfilePage',
+                component: ProfilePage,
+            },
+            {
+                path: '/places/list',
+                name: 'PlacesList',
+                component: PlacesList
+            }
+        ]),
+        ...middleware(guest(store))([
+            {
+                path: '/login',
+                name: 'Login',
+            }
+        ])
     ]
 });
