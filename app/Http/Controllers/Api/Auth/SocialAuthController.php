34379481f8c8ac1a208dac21ecfcadea87d57572
Лесогорский Кирилл <lesogor.kirill@gmail.com>
2018-08-09 17:43:58 +0300

    created controller

---
+++ app/Http/Controllers/Api/Auth/SocialAuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hedonist\Http\Controllers\Api\Auth;
+
+
+use Hedonist\Http\Controllers\Api\ApiController;
+
+class SocialAuthController extends ApiController
+{
+    public function redirect(string $provider)
+    {
+
+    }
+
+    public function callback(string $provider)
+    {
+
+    }
+}
