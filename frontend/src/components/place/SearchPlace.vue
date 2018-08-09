32fb5acc167bfb43c4acb96e4a1d363df021a723
aLLeXUs <mrallexus@gmail.com>
2018-08-09 21:59:48 +0300

    Add map to layout

--- frontend/src/components/place/SearchPlace.vue
+++ frontend/src/components/place/SearchPlace.vue
@@ -6,8 +6,23 @@
                 <PlaceListComponent :key="place.id" :place="place" :timer="50 * (index+1)"/>
             </template>
         </section>
-        <section class="column mapbox">
-
+        
+        <section id="map" class="column mapbox">
+            <mapbox
+                access-token="pk.eyJ1IjoiYWxsZXh1cyIsImEiOiJjamttdGE1cmIxM2htM2xtZzQxZXJubGdrIn0.KgivvenlaQUbX8cQxLNiWw"
+                :map-options="{
+                style: 'mapbox://styles/mapbox/light-v9',
+                zoom: 3
+                }"
+                :scale-control="{
+                show: true,
+                position: 'top-left'
+                }"
+                :fullscreen-control="{
+                show: true,
+                position: 'top-left'
+                }">
+            </mapbox>
         </section>
     </section>
 </template>
@@ -16,11 +31,13 @@
     import { mapState } from "vuex";
     import { mapGetters } from "vuex";
     import PlaceListComponent from '@/components/PlacesList/PlaceListComponent';
+    import Mapbox from 'mapbox-gl-vue';
 
     export default {
         name: "SearchPlace",
         components: {
-            PlaceListComponent
+            PlaceListComponent,
+            Mapbox,
         },
         data() {
             return {
@@ -53,5 +70,11 @@
     .columns {
         padding-left: 10px;
         padding-right: 10px;
+    }
+
+    #map { 
+        width: 100%;
+        height: 100%;
+        min-height: 500px;
     }
 </style>
