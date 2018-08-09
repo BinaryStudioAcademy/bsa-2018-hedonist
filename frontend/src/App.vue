e0f3af7ab5b0c42bb9c5d961f1ec661eeb1998ed
k0sen <caladien12@gmail.com>
2018-08-09 11:23:34 +0300

    feature/place_layout_component: add route navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,14 @@
             href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
             integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
             crossorigin="anonymous">
+        <nav class="navigation">
+            <ul class="navbar">
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/profile">Profile</router-link></li>
+                <li><router-link to="/place-info">Place-info</router-link></li>
+                <li><router-link to="/places/list">Place-list</router-link></li>
+            </ul>
+        </nav>
         <router-view/>
     </div>
 </template>
@@ -24,5 +32,9 @@ export default {
     color: #4e595d;
     background-color: #efeff4;
     margin-top: 60px;
+}
+
+.navigation .navbar li {
+    margin-right: 15px;
 }
 </style>
