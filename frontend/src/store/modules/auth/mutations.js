55b2f0b197d2850c2ea77162659fa81341a0bbca
Alexander Kharchenko <sandrokharchenko@gmail.com>
2018-08-09 17:51:22 +0300

    auth: fix login mutations

--- frontend/src/store/modules/auth/mutations.js
+++ frontend/src/store/modules/auth/mutations.js
@@ -8,11 +8,13 @@ export default {
         StorageService.setToken(response.token);
         state.token = response.token;
         state.currentUser = response.user;
+        state.isLoggedIn = true;
     },
     USER_LOGOUT: (state) => {
         StorageService.removeToken();
         state.token = '';
         state.currentUser = null;
+        state.isLoggedIn = false;
     },
     SET_AUTHENTICATED_USER: (state, user) => {
         state.currentUser = user;
