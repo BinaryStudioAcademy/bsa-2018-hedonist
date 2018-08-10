aad4aed1e263958dd80eacec577bde5c7c739a3f
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-08-10 10:18:08 +0300

    Reject creating ReviewPage.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import ProfilePage from '@/pages/ProfilePage';
-import ReviewPage from '@/pages/ReviewPage';
+import ReviewList from '@/components/review/ReviewList';
 
 Vue.use(Router);
 
@@ -20,8 +20,8 @@ export default new Router({
     },
     {
       path: '/reviews',
-      name: 'ReviewPage',
-      component: ReviewPage
+      name: 'ReviewList',
+      component: ReviewList
     }
   ]
 });
