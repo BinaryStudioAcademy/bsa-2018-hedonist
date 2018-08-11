1a65196d8c7cceab651a740d07b42fe9b4b58522
Lesohorskyi Kirill <lesogor.kirill@gmail.com>
2018-08-11 20:38:36 +0300

    fixed action

--- app/Actions/Auth/GetUserAction.php
+++ app/Actions/Auth/GetUserAction.php
@@ -26,8 +26,8 @@ class GetUserAction
     {
         $user = $this->userRepository->getById($request->getId());
         $userInfo = $this->infoRepository->getByUserId($request->getId());
-        if(is_null($user) || is_null($userInfo)){//if there are no user or user data, then throw exception
-            throw new InvalidUserDataException();
+        if(is_null($user) || is_null($userInfo)){
+            throw InvalidUserDataException::create();
         }
 
         return new GetUserResponse($user,$userInfo);
--- app/Exceptions/Auth/InvalidUserDataException.php
+++ app/Exceptions/Auth/InvalidUserDataException.php
@@ -4,7 +4,8 @@ namespace Hedonist\Exceptions\Auth;
 
 class InvalidUserDataException extends \Exception
 {
-   public static function create(){
+   public static function create(): self
+   {
        return new self("Sorry, looks like your data is corrupted. Please, contact our customer support");
    }
 }
