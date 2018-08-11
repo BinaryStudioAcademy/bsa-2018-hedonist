0b76a2c2e9db3fb67388c972c9c8f0035ede5835
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-08-11 22:52:16 +0300

    Start creating NewPlacePage.

---
+++ frontend/src/pages/NewPlacePage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="main-part column is-two-thirds">
+                <div class="name-row">
+                    <span>Name:</span>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input">
+                    </div>
+                </div>
+            </div>
+            <div class="map-part column">
+                <div class="map-box">
+                    <img src="https://a.tiles.mapbox.com/v3/foursquare.qhb8olxr/16/38326/22101.png" alt="mapbox">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewPlacePage"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .map-box {
+        padding: 10px;
+
+        img {
+            width: 100%;
+        }
+    }
+
+</style>
