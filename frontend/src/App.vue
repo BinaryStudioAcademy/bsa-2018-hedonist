4e12b9daeea46f72a196b7534d0450e89eacd53a
MihailTS <tseluiko.m@gmail.com>
2018-08-12 05:36:25 +0300

    top_navbar_mockup: add navbar component. Remove navigation from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,18 @@
 <template>
    <div id="app">
-        <nav class="navigation">
-            <ul class="navbar">
-                <li><router-link to="/">Home</router-link></li>
-                <li><router-link to="/signup">Sign up</router-link></li>
-                <li><router-link to="/login">Login</router-link></li>
-                <li><a @click="onLogOut">Logout</a></li>
-                <li><router-link to="/profile">Profile</router-link></li>
-                <li><router-link to="/place-info">Place-info</router-link></li>
-                <li><router-link to="/places/list">Place-list</router-link></li>
-                <li><router-link to="/tastes/add">Tastes add</router-link></li>
-                <li><router-link to="/user/lists">User lists</router-link></li>
-            </ul>
-        </nav>
-        <router-view/>
+       <TopNavbar/>
+
+       <router-view/>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import TopNavbar from './components/navbar/TopNavbar';
 
 export default {
     name: 'App',
-
-    methods: {
-        ...mapActions(['logout']),
-
-        onLogOut () {
-            this.logout();
-        }
+    components:{
+        TopNavbar
     }
 }
 </script>
@@ -55,8 +39,4 @@ html, body {
     -moz-osx-font-smoothing: grayscale;
 }
 
-.navigation .navbar li {
-    line-height: 50px;
-    margin-right: 15px;
-}
 </style>
---
+++ frontend/src/components/navbar/TopNavbar.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="is-paddingless">
+        <nav class="navbar is-info">
+            <div class="navbar-wrapper container is-flex">
+                <div class="navbar-brand navbar-brand-name">
+                    <router-link class="navbar-item" to="/">Hedonist</router-link>
+                </div>
+
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        <div class="navbar-item">
+                            <div class="control has-icons-right">
+                                <input class="input" type="search" placeholder="I'm looking for...">
+                                <span class="icon is-right">
+                                    <i class="fas fa-caret-down"></i>
+                               </span>
+                            </div>
+                        </div>
+                        <div class="navbar-item">
+                            <div class="control">
+                                <input class="input" type="search" value="Lviv, UA">
+                            </div>
+                        </div>
+                        <div class="navbar-item is-paddingless navbar-search-btn">
+                           <span class="icon is-large">
+                               <i class="fas fa-lg fa-search"></i>
+                           </span>
+                        </div>
+
+                    </div>
+
+                    <div v-if="!this.isUserLoggedIn" class="navbar-end">
+                        <router-link class="navbar-item" to="/login">Log In</router-link>
+                        <router-link class="navbar-item" to="/signup">Sign Up</router-link>
+                    </div>
+                    
+                    <div v-if="this.isUserLoggedIn" class="navbar-end">
+                        <div class="navbar-item is-paddingless">
+                            <span class="navbar-notification-btn"></span>
+                        </div>
+                        <div  class="navbar-item has-dropdown is-hoverable">
+                            <div class="navbar-link navbar-dropdown-menu">
+                                <img class="navbar-avatar" :src="user.avatarUrl"
+                                     :title="user.firstName+' '+user.lastName"
+                                     :alt="user.firstName+' '+user.lastName">
+                                <span>{{user.firstName}}</span>
+                                <span class="icon">
+                                    <i class="fas fa-caret-down"></i>
+                               </span>
+                            </div>
+                            <div class="navbar-dropdown">
+                                <router-link class="navbar-item" to="/profile">Profile</router-link>
+                                <router-link class="navbar-personal-link navbar-item" to="/places/list">My places</router-link>
+                                <router-link class="navbar-personal-link navbar-item" to="/user/lists">My lists</router-link>
+                                <a class="navbar-item" @click="onLogOut">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+</template>
+
+<script>
+    import { mapActions, mapGetters } from 'vuex';
+
+    export default {
+        name: "TopNavbar",
+        computed: mapGetters({isUserLoggedIn:"isLoggedIn"}),
+        data() {
+            return {
+                user: {
+                    firstName: 'John',
+                    lastName: 'Carter',
+                    avatarUrl:'http://via.placeholder.com/200x200'
+                }
+            }
+        },
+        methods: {
+            ...mapActions(['logout']),
+
+            onLogOut () {
+                this.logout();
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .navbar-wrapper{
+        width:1060px;
+    }
+    .navbar-brand-name{
+        text-transform: uppercase;
+        font-weight: bold;
+        letter-spacing: 0.2rem;
+    }
+    .navbar-search-btn{
+        cursor:pointer;
+    }
+    .navbar-dropdown-menu{
+        padding-right: .75rem;
+        &:after{
+            border: none;
+        }
+    }
+    .navbar-personal-link{
+        text-indent: 15px;
+    }
+    .navbar-avatar{
+        margin:0 10px;
+        border-radius:4px;
+    }
+    .navbar-notification-btn{
+        cursor:pointer;
+        background: url("https://foursquare.com/img/chrome/icon-notifications_foursquare8.png") top left no-repeat;
+        font-weight: bold;
+        height: 24px;
+        width: 27px;
+        align-self: center;
+    }
+</style>
