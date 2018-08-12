b8d755a423c2aec4a97231f96f072fad11781802
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-08-12 17:00:55 +0300

    Overall view created.

--- frontend/src/pages/NewPlacePage.vue
+++ frontend/src/pages/NewPlacePage.vue
@@ -1,11 +1,102 @@
 <template>
-    <div class="container">
-        <div class="columns">
+    <div class="section">
+        <div class="container main-wrp columns">
             <div class="main-part column is-two-thirds">
-                <div class="name-row">
-                    <span>Name:</span>
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Text input">
+                <div class="field is-horizontal">
+                    <div class="field-label is-medium">
+                        <label class="label">Name</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <div class="control is-expanded">
+                                <input class="input is-medium" type="text" placeholder="Place's name">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <h4>Address and Location</h4>
+
+                <div class="field is-horizontal">
+                    <div class="field-label is-normal">
+                        <label class="label">City</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="text" placeholder="Place's city">
+                            </p>
+                        </div>
+                        <div class="field-label is-normal">
+                            <label class="label">Zip</label>
+                        </div>
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="text" placeholder="Place's postal code">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="field is-horizontal">
+                    <div class="field-label is-normal">
+                        <label class="label">Address</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <div class="control is-expanded">
+                                <input class="input" type="text" placeholder="Place's address">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <h4>Contact and Other Info</h4>
+
+                <div class="field is-horizontal">
+                    <div class="field-label is-normal">
+                        <label class="label">Phone</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="tel" placeholder="Place's phone">
+                            </p>
+                        </div>
+                        <div class="field-label is-normal">
+                            <label class="label">Website</label>
+                        </div>
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="text" placeholder="Place's website">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="field is-horizontal">
+                    <div class="field-label is-normal">
+                        <label class="label">Description</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <div class="control">
+                                <textarea class="textarea" placeholder="Place's description.."></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="field is-horizontal">
+                    <div class="field-label"></div>
+                    <div class="field-body">
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-primary">
+                                    Add place
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -26,9 +117,28 @@
 
 <style lang="scss" scoped>
 
-    .map-box {
-        padding: 10px;
+    .section {
+        padding-top: 24px;
+    }
 
+    .main-wrp {
+        margin: auto;
+    }
+
+    .name-block {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        span {
+            text-align: center;
+        }
+    }
+
+
+
+    .map-box {
         img {
             width: 100%;
         }
