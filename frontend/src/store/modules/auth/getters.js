b8c3ad346778cef99bd03758226e50e2318acd93
k0sen <caladien12@gmail.com>
2018-08-13 14:49:26 +0300

    hotfix/move_token_to_local-storage: fixed

--- frontend/src/store/modules/auth/getters.js
+++ frontend/src/store/modules/auth/getters.js
@@ -1,10 +1,7 @@
 import StorageService from '@/services/common/storageService';
 
 export default {
-    isLoggedIn: (state) => {
-        let stub = state.token;
-        return StorageService.hasToken();
-    },
-    getAuthenticatedUser: () => StorageService.getAuthenticatedUser(),
+    hasToken: () => () => StorageService.hasToken(),
+    getAuthenticatedUser: () => () => StorageService.getAuthenticatedUser(),
     getToken: () => StorageService.getToken(),
 };
