0e6665893882a261052c2a364faf50e7b5a093b6
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-08-14 18:55:42 +0300

    Change dir's name.

--- frontend/src/store/modules/userList/mutations.js
+++
@@ -1,5 +0,0 @@
-export default {
-    SET_LIST: (state, data) => {
-        state.lists = data;
-    }
-}
--- frontend/src/store/modules/userlist/getters.js
+++
@@ -1,5 +0,0 @@
-export default {
-    getLists: state => {
-        return state.lists;
-    }
-}
