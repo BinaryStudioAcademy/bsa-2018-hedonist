41a999634d1fb68b069bdacda24a7da22efbf28f
k0sen <caladien12@gmail.com>
2018-08-14 12:18:13 +0300

    fix/auth-flow: change exception handling

--- app/Http/Middleware/JwtAuthenticate.php
+++ app/Http/Middleware/JwtAuthenticate.php
@@ -6,30 +6,42 @@ use Closure;
 use Hedonist\Actions\Auth\Presenters\AuthPresenter;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
-use Tymon\JWTAuth\Http\Middleware\Authenticate;
 use Tymon\JWTAuth\JWTAuth;
 
-class JwtAuthenticate extends Authenticate
+class JwtAuthenticate
 {
+    private $auth;
+
+    public function __construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
     public function handle($request, Closure $next)
     {
+        $this->auth->parser()->setRequest($request);
+
         try {
-            $this->authenticate($request);
-        } catch (UnauthorizedHttpException $exception) {
-            if ($exception->getPrevious() instanceof TokenExpiredException) {
-                return $this->tokenExpireResponse(AuthPresenter::presentError($exception));
+            if (! $this->auth->parser()->hasToken()) {
+                throw new UnauthorizedHttpException('jwt-auth', 'Token not provided');
             }
 
-            return $this->errorResponse(
-                AuthPresenter::presentError($exception),
-                401
-            );
+            if (! $this->auth->parseToken()->authenticate()) {
+                throw new UnauthorizedHttpException('jwt-auth', 'User not found');
+            }
+        } catch (TokenExpiredException $exception) {
+            return $this->tokenExpireResponse(AuthPresenter::presentError($exception));
+        } catch (UnauthorizedHttpException $exception) {
+            return $this->errorResponse(AuthPresenter::presentError($exception));
+        } catch (JWTException $exception) {
+            return $this->errorResponse(AuthPresenter::presentError($exception));
         }
         return $next($request);
     }
 
-    protected function tokenExpireResponse(string $data, int $statusCode = 401) : JsonResponse
+    protected function tokenExpireResponse(string $data, int $statusCode = JsonResponse::HTTP_UNAUTHORIZED) : JsonResponse
     {
         $errorData = [
             'error' => [
@@ -41,7 +53,7 @@ class JwtAuthenticate extends Authenticate
         return JsonResponse::create($errorData, $statusCode);
     }
 
-    protected function errorResponse(string $data, int $statusCode = JsonResponse::HTTP_BAD_REQUEST) : JsonResponse
+    protected function errorResponse(string $data, int $statusCode = JsonResponse::HTTP_UNAUTHORIZED) : JsonResponse
     {
         $errorData = [
             'error' => [
