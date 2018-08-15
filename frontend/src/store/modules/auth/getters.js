216ea94b81469b92797a42f7e678df6603ca764d
k0sen <caladien12@gmail.com>
2018-08-15 16:33:49 +0300

    fix/auth-flow: return back getAuthenticatedUser

--- frontend/src/store/modules/auth/getters.js
+++ frontend/src/store/modules/auth/getters.js
@@ -2,5 +2,6 @@ import StorageService from '@/services/common/storageService';
 
 export default {
     hasToken: () => () => StorageService.hasToken(),
+    getAuthenticatedUser: (state) => state.currentUser,
     getToken: () => () => StorageService.getToken(),
 };
