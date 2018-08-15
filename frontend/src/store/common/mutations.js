f186d348f3f3af9185b4a28228c8b73568a35c65
k0sen <caladien12@gmail.com>
2018-08-15 10:19:55 +0300

    fix/auth-flow: remove setError & getError mutations

--- frontend/src/store/common/mutations.js
+++ frontend/src/store/common/mutations.js
@@ -2,11 +2,5 @@ export default {
     SET_LOADING: (state, isLoading) => {
         state.loading = isLoading;
     },
-    setError: (state, error) => {
-        state.error = error;
-    },
-    clearError: state => {
-        state.error = null;
-    }
 };
 
