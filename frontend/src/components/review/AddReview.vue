ee7ee2f9f6bd7e29846cf740a5b70548051fb109
aLLeXUs <mrallexus@gmail.com>
2018-08-17 00:13:14 +0300

    Add review default avatar

--- frontend/src/components/review/AddReview.vue
+++ frontend/src/components/review/AddReview.vue
@@ -5,7 +5,7 @@
 
                 <div class="media-left">
                     <img
-                        src="https://ss0.4sqi.net/img/venuepage/v2/add_tip_blank_avatar@2x-4321684c656168f26ae9208901a9d83e.png"
+                        src="/assets/add_review_default_avatar.png"
                         height="32"
                         width="32"
                     >
@@ -53,7 +53,7 @@
         <div v-else class="add-review media">
             <div class="media-left">
                 <img
-                    src="https://ss0.4sqi.net/img/venuepage/v2/add_tip_blank_avatar@2x-4321684c656168f26ae9208901a9d83e.png"
+                    src="/assets/add_review_default_avatar.png"
                     height="32"
                     width="32"
                 >
@@ -72,8 +72,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { mapActions } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
     name: 'AddReview',
