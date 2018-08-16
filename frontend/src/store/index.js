402b216c9655f8c96629911f1c8b973d3e99f2df
MihailTS <tseluiko.m@gmail.com>
2018-08-16 22:25:40 +0300

    navbar_search_city: remove index from imports in store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,11 +4,11 @@ import state from './common/state';
 import actions from './common/actions';
 import getters from './common/getters';
 import mutations from './common/mutations';
-import auth from './modules/auth/index';
-import city from './modules/city/index';
-import place from './modules/place/index';
-import placeCategory from './modules/place/category/index';
-import map from './modules/map/index';
+import auth from './modules/auth';
+import city from './modules/city';
+import place from './modules/place';
+import placeCategory from './modules/place/category';
+import map from './modules/map';
 
 Vue.use(Vuex);
 
