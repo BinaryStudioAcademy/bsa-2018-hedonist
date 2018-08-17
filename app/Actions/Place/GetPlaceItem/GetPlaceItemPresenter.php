242e8c73c86c963f97314ddc1fe16999ad549bfa
MihailTS <tseluiko.m@gmail.com>
2018-08-17 15:46:06 +0300

    place: fix codestyle

--- app/Actions/Place/GetPlaceItem/GetPlaceItemPresenter.php
+++ app/Actions/Place/GetPlaceItem/GetPlaceItemPresenter.php
@@ -7,15 +7,15 @@ class GetPlaceItemPresenter
     public static function present(GetPlaceItemResponse $placeResponse): array
     {
         $localization = null;
-        if($placeResponse->getLocalization()->isNotEmpty()){
+        if ($placeResponse->getLocalization()->isNotEmpty()) {
             $localization = array_reduce(
                 $placeResponse->getLocalization()->toArray(),
                 function ($carry, $localizationItem) {
                     $localizationPresenter = [
-                        'id'          => $localizationItem['id'],
-                        'name'        => $localizationItem['place_name'],
+                        'id' => $localizationItem['id'],
+                        'name' => $localizationItem['place_name'],
                         'description' => $localizationItem['place_description'],
-                        'languageId'  => $localizationItem['language_id']
+                        'languageId' => $localizationItem['language_id']
                     ];
                     $carry[$localizationItem['language']['code']] = $localizationPresenter;
                     return $carry;
@@ -25,22 +25,22 @@ class GetPlaceItemPresenter
         }
 
         return [
-            'id'           => $placeResponse->getId(),
-            'latitude'     => $placeResponse->getLatitude(),
-            'longitude'    => $placeResponse->getLongitude(),
-            'zip'          => $placeResponse->getZip(),
-            'address'      => $placeResponse->getAddress(),
-            'phone'        => $placeResponse->getPhone(),
-            'website'      => $placeResponse->getWebsite(),
-            'creatorId'    => $placeResponse->getCreatorId(),
-            'createdAt'    => $placeResponse->getCreatedAt(),
-            'updatedAt'    => $placeResponse->getUpdatedAt(),
+            'id' => $placeResponse->getId(),
+            'latitude' => $placeResponse->getLatitude(),
+            'longitude' => $placeResponse->getLongitude(),
+            'zip' => $placeResponse->getZip(),
+            'address' => $placeResponse->getAddress(),
+            'phone' => $placeResponse->getPhone(),
+            'website' => $placeResponse->getWebsite(),
+            'creatorId' => $placeResponse->getCreatorId(),
+            'createdAt' => $placeResponse->getCreatedAt(),
+            'updatedAt' => $placeResponse->getUpdatedAt(),
             'localization' => $localization,
             'reviews' => $placeResponse->getReviews()->isNotEmpty()
                 ? array_map(function ($review) {
                     return [
-                        'id'          => $review['id'],
-                        'userId'      => $review['user_id'],
+                        'id' => $review['id'],
+                        'userId' => $review['user_id'],
                         'description' => $review['description']
                     ];
                 }, $placeResponse->getReviews()->toArray())
@@ -48,17 +48,17 @@ class GetPlaceItemPresenter
             'features' => $placeResponse->getFeatures()->isNotEmpty()
                 ? array_map(function ($feature) {
                     return [
-                        'id'   => $feature['id'],
+                        'id' => $feature['id'],
                         'name' => $feature['name'],
                     ];
                 }, $placeResponse->getFeatures()->toArray())
                 : null,
             'category' => [
-                'id'   => $placeResponse->getCategory()->id,
+                'id' => $placeResponse->getCategory()->id,
                 'name' => $placeResponse->getCategory()->name
             ],
             'city' => [
-                'id'   => $placeResponse->getCity()->id,
+                'id' => $placeResponse->getCity()->id,
                 'name' => $placeResponse->getCity()->name
             ],
             'rating' => $placeResponse->getRating(),
