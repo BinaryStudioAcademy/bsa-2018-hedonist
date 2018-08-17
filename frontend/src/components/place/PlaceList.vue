b7ecca1c6fa5dda73414d32cdc1fd57514de0e39
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2018-08-17 18:10:57 +0300

    add link for add place

--- frontend/src/components/place/PlaceList.vue
+++ frontend/src/components/place/PlaceList.vue
@@ -1,13 +1,19 @@
 <template>
     <section class="container">
+        <div class="action-bar">
+            <router-link
+                    class="button is-success"
+                    :to="{ name: 'NewPlacePage'}"
+            >Create a new place page</router-link>
+        </div>
         <ul>
-            <li 
-                v-for="(place,index) in places" 
-                :key="place.id"
+            <li
+                    v-for="(place,index) in places"
+                    :key="place.id"
             >
                 <PlacePreview
-                    :place="place" 
-                    :timer="50 * (index+1)"
+                        :place="place"
+                        :timer="50 * (index+1)"
                 />
             </li>
         </ul>
@@ -31,19 +37,24 @@
                 }
             }
         }
+
+        .action-bar {
+            padding: 20px 0;
+            text-align: right;
+        }
     }
 </style>
 
 <script>
-import PlacePreview from './PlacePreview';
-import { mapState } from 'vuex';
+    import PlacePreview from './PlacePreview';
+    import { mapState } from 'vuex';
 
-export default {
-    name: 'PlaceList',
-    components: {PlacePreview},
-    created() {
-        this.$store.dispatch('place/fetchPlaces');
-    },
-    computed: {...mapState('place', ['places'])}
-};
+    export default {
+        name: 'PlaceList',
+        components: {PlacePreview},
+        created() {
+            this.$store.dispatch('place/fetchPlaces');
+        },
+        computed: {...mapState('place', ['places'])}
+    };
 </script>
