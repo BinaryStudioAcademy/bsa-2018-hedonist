35700b7ee32f7239d515cff72c982c9a2f23b121
Lesohorskyi Kirill <lesogor.kirill@gmail.com>
2018-08-18 16:42:59 +0300

    fix

--- frontend/src/components/place/PlacePreview.vue
+++ frontend/src/components/place/PlacePreview.vue
@@ -9,9 +9,8 @@
                 <div class="media-content">
                     <h3
                         class="title has-text-primary"
-                        v-if="place.localization[0]"
                     >
-                        <router-link :to=`/places/${place.id}`>
+                        <router-link :to="`/places/${place.id}`">
                             {{ localizedName }}
                         </router-link>
                     </h3>
