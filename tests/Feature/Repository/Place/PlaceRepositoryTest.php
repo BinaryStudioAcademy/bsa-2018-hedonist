7db0d4ee71db4b958abc51a4cf48b161de088c74
aLLeXUs <mrallexus@gmail.com>
2018-08-18 13:47:00 +0300

    Add testFindByCoordinatesNotFound

--- tests/Feature/Repository/Place/PlaceRepositoryTest.php
+++ tests/Feature/Repository/Place/PlaceRepositoryTest.php
@@ -5,11 +5,13 @@ namespace Tests\Feature\Repository\Place;
 use Hedonist\Entities\Place\Location;
 use Hedonist\Entities\Place\Place;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PlaceRepositoryTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
     private $placeRepository;
 
@@ -18,6 +20,17 @@ class PlaceRepositoryTest extends TestCase
         parent::setUp();
 
         $this->placeRepository = app(\Hedonist\Repositories\Place\PlaceRepository::class);
+        factory(Place::class, 10)->create([
+            'longitude' => $this->faker->randomFloat(4, 10, 50),
+            'latitude' => $this->faker->randomFloat(4, 10, 50),
+        ]);
+    }
+
+    public function testFindByCoordinatesNotFound()
+    {
+        $location = new Location(100, 90);
+        $result = $this->placeRepository->findByCoordinates($location, 1);
+        $this->assertEmpty($result);
     }
 
     public function testFindByCoordinates()
