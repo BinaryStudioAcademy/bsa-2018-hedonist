9948ca7e5fb95dbc686dd8c534f915b78f7dd560
Lesohorskyi Kirill <lesogor.kirill@gmail.com>
2018-08-19 09:55:00 +0300

    WIP

--- frontend/src/components/place/PlaceTopInfo.vue
+++ frontend/src/components/place/PlaceTopInfo.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="place-top-info">
-        <PlacePhotoList />
+        <PlacePhotoList/>
         <div class="place-venue columns">
             <div class="column is-two-thirds">
                 <div class="place-venue__logo">
                     <img
-                        src="https://ss3.4sqi.net/img/categories_v2/food/caucasian_88.png"
-                        data-retina-url="https://ss3.4sqi.net/img/categories_v2/food/caucasian_512.png"
-                        width="88"
-                        height="88"
+                            src="https://ss3.4sqi.net/img/categories_v2/food/caucasian_88.png"
+                            data-retina-url="https://ss3.4sqi.net/img/categories_v2/food/caucasian_512.png"
+                            width="88"
+                            height="88"
                     >
                 </div>
                 <div class="place-venue__prime-info">
@@ -21,29 +21,30 @@
                 </div>
             </div>
             <div class="column is-one-third place-venue__actions">
-                <button 
-                    class="button is-primary"
-                    @click="isCheckinModalActive = true"
+                <button
+                        class="button is-primary"
+                        @click="isCheckinModalActive = true"
                 >
-                    <i class="fas fa-check" />Check-in
+                    <i class="fas fa-check"/>Check-in
                 </button>
                 <b-modal :active.sync="isCheckinModalActive" has-modal-card>
-                    <PlaceCheckinModal :place="place" />
+                    <PlaceCheckinModal :place="place"/>
                 </b-modal>
 
                 <b-dropdown>
                     <button class="button is-success" slot="trigger">
-                        <i class="far fa-save" />Save
-                        <b-icon icon="menu-down" />
+                        <i class="far fa-save"/>Save
+                        <b-icon icon="menu-down"/>
                     </button>
 
                     <template v-for="list in userlist">
-                        <b-dropdown-item :key="list.id" @click="addPlaceToList(list.id)">{{ list.name }}</b-dropdown-item>
+                        <b-dropdown-item :key="list.id" @click="addPlaceToList(list.id)">{{ list.name }}
+                        </b-dropdown-item>
                     </template>
                 </b-dropdown>
 
                 <button class="button is-info">
-                    <i class="far fa-share-square" />Share
+                    <i class="far fa-share-square"/>Share
                 </button>
             </div>
         </div>
@@ -52,14 +53,14 @@
                 <nav class="sidebar-actions tabs">
                     <ul>
                         <li
-                            @click="changeTab(1)"
-                            :class="{ 'is-active' : activeTab === 1}"
+                                @click="changeTab(1)"
+                                :class="{ 'is-active' : activeTab === 1}"
                         >
                             <a><span>Reviews (2)</span></a>
                         </li>
                         <li
-                            @click="changeTab(2)"
-                            :class="{ 'is-active' : activeTab === 2}"
+                                @click="changeTab(2)"
+                                :class="{ 'is-active' : activeTab === 2}"
                         >
                             <a><span>Photos (12)</span></a>
                         </li>
@@ -72,17 +73,11 @@
                 </div>
                 <div class="place-rate__mark-count">444 marks</div>
                 <div class="place-rate__preference">
-                    <div class="likable like">
-                        <span class="fa-stack fa-2x">
-                            <i class="fa fa-heart fa-stack-1x" />
-                        </span>
-                    </div>
-                    <div class="likable dislike">
-                        <span class="fa-stack fa-2x">
-                            <i class="fa fa-heart fa-stack-1x" />
-                            <i class="fa fa-bolt fa-stack-1x fa-inverse" />
-                        </span>
-                    </div>
+                    <LikeDislikeButtons
+                            :likes="place.likes"
+                            :dislikes="place.dislikes"
+                            :like="NONE"
+                    />
                 </div>
             </div>
         </div>
@@ -90,56 +85,58 @@
 </template>
 
 <script>
-import PlacePhotoList from './PlacePhotoList';
-import PlaceCheckinModal from './PlaceCheckinModal';
+    import PlacePhotoList from './PlacePhotoList';
+    import PlaceCheckinModal from './PlaceCheckinModal';
+    import LikeDislikeButtons from '@/components/misc/LikeDislikeButtons';
 
-export default {
-    name: 'PlaceTopInfo',
-    components: {
-        PlacePhotoList,
-        PlaceCheckinModal
-    },
-    props: {
-        place: {
-            type: Object,
-            required: true
-        }
-    },
-    data() {
-        return {
-            activeTab: 1,
-            userlist: {},
-            isCheckinModalActive: false
-        };
-    },
-
-    created() {
-        this.$store.dispatch('userlist/getListsByUser', this.user.id)
-            .then((result) => {
-                this.userlist = result;
-            });
-    },
-
-    computed: {
-        user() {
-            return this.$store.getters['auth/getAuthenticatedUser'];
-        }
-    },
-
-    methods: {
-        changeTab: function(activeTab) {
-            this.activeTab = activeTab;
-            this.$emit('tabChanged', activeTab);
+    export default {
+        name: 'PlaceTopInfo',
+        components: {
+            PlacePhotoList,
+            PlaceCheckinModal,
+            LikeDislikeButtons
+        },
+        props: {
+            place: {
+                type: Object,
+                required: true
+            }
+        },
+        data() {
+            return {
+                activeTab: 1,
+                userlist: {},
+                isCheckinModalActive: false
+            };
         },
 
-        addPlaceToList: function (listId) {
-            this.$store.dispatch('userlist/addPlaceToList', {
-                listId: listId,
-                placeId: this.place.id
-            });
+        created() {
+            this.$store.dispatch('userlist/getListsByUser', this.user.id)
+                .then((result) => {
+                    this.userlist = result;
+                });
+        },
+
+        computed: {
+            user() {
+                return this.$store.getters['auth/getAuthenticatedUser'];
+            }
+        },
+
+        methods: {
+            changeTab: function (activeTab) {
+                this.activeTab = activeTab;
+                this.$emit('tabChanged', activeTab);
+            },
+
+            addPlaceToList: function (listId) {
+                this.$store.dispatch('userlist/addPlaceToList', {
+                    listId: listId,
+                    placeId: this.place.id
+                });
+            }
         }
-    }
-};
+    };
 </script>
 
 <style lang="scss" scoped>
@@ -186,12 +183,13 @@ export default {
                     .fa-bolt {
                         top: -5%;
                         left: 2%;
-                        font-size:70%;
+                        font-size: 70%;
                     }
                 }
             }
         }
     }
+
     .place-venue {
         margin: 20px;
         &__logo {
@@ -233,6 +231,7 @@ export default {
             }
         }
     }
+
     @media screen and (max-width: 370px) {
         .place-venue {
             &__actions {
