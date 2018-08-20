fb19ece0b8de7827ae41a6bb027c580ce7812e36
MihailTS <tseluiko.m@gmail.com>
2018-08-20 18:15:41 +0300

    navbar_place_categories_search: change place category namespace

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,6 +6,7 @@ import getters from './common/getters';
 import mutations from './common/mutations';
 import auth from './modules/auth';
 import search from './modules/search';
+import category from './modules/category';
 import place from './modules/place';
 import map from './modules/map';
 import review from './modules/review';
@@ -21,6 +22,7 @@ export default new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',
     modules: {
         auth,
+        category,
         map,
         place,
         search,
