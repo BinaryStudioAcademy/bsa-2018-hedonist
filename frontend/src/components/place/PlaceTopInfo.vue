2ca8d5b4e7eaa9d85672ec22ae3c7de649820e88
Lesohorskyi Kirill <lesogor.kirill@gmail.com>
2018-08-21 20:01:17 +0300

    like status changed

--- frontend/src/components/place/PlaceTopInfo.vue
+++ frontend/src/components/place/PlaceTopInfo.vue
@@ -76,7 +76,7 @@
                     <LikeDislikeButtons
                             :likes="place.likes"
                             :dislikes="place.dislikes"
-                            like="liked"
+                            like="NONE"
                             @like="like"
                             @dislike="dislike"
                     />
