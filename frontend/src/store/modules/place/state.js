22b8e827221a428a28ce8990082093e775239c7a
ivanshamis <ivanshamis@gmail.com>
2018-08-23 19:48:51 +0300

    place-map-marker: clear

--- frontend/src/store/modules/place/state.js
+++ frontend/src/store/modules/place/state.js
@@ -3,7 +3,6 @@ import { STATUS_NONE } from '@/services/api/codes';
 export default {
     places: [],
     currentPlace: null,
-    liked: STATUS_NONE,
     mapInitialized : false,
     currentPlaceReviews: null,
     liked: STATUS_NONE
