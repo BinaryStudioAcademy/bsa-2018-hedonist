15c312f1ae6ce4d84f31c589974627221b9bf230
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-08-23 10:59:21 +0300

    SearchCategoryIcon : move it to storage on the server

--- app/Entities/Place/PlaceCategory.php
+++ app/Entities/Place/PlaceCategory.php
@@ -18,10 +18,7 @@ class PlaceCategory extends Model
 
     protected $table = 'place_categories';
 
-    protected $fillable = [
-        'name',
-        'logo'
-        ];
+    protected $fillable = ['name','logo'];
 
     protected $dates = ['deleted_at'];
 
