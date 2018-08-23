0012c307bad80f6f64be5ad7d1bbd3d077384551
ivanshamis <ivanshamis@gmail.com>
2018-08-23 19:47:13 +0300

    place-map-marker: clear

--- frontend/src/store/modules/place/actions.js
+++ frontend/src/store/modules/place/actions.js
@@ -179,10 +179,6 @@ export default {
             });
     },
 
-    setCurrentCenter: ({ commit }, currentCenter) => {
-        commit('SET_CURRENT_CENTER', currentCenter);
-    },
-
     mapInitialization: ({ commit }) => {
         commit('MAP_INIT', true);
     }
