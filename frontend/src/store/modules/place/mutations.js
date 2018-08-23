3dd331266cd29908834af6391b85d6bdca3ce6aa
ivanshamis <ivanshamis@gmail.com>
2018-08-23 19:50:48 +0300

    place-map-marker: clear

--- frontend/src/store/modules/place/mutations.js
+++ frontend/src/store/modules/place/mutations.js
@@ -27,11 +27,6 @@ export default {
         state.currentPlace.ratingCount = ratingCount;
     },
 
-    SET_CURRENT_CENTER: (state, { longitude, latitude }) => {
-        state.currentLatitude = latitude;
-        state.currentLongitude = longitude;
-    },
-
     MAP_INIT: (state, value) => {
         state.mapInitialized = value;
     },
