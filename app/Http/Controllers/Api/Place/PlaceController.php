12e93c285628f44dc947d31b489f60701a26b1fc
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2018-08-23 11:57:42 +0300

    fix mistakes

--- app/Http/Controllers/Api/Place/PlaceController.php
+++ app/Http/Controllers/Api/Place/PlaceController.php
@@ -140,9 +140,7 @@ class PlaceController extends ApiController
             );
 
             return $this->successResponse($presenter->present($placeResponse), 200);
-        } catch (DomainException $e) {
-            return $this->errorResponse($e->getMessage());
-        } catch (\InvalidArgumentException $e) {
+        } catch (DomainException | \InvalidArgumentException $e) {
             return $this->errorResponse($e->getMessage());
         }
     }
