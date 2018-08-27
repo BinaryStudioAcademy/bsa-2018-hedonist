566e9d42fb817bf970e11ff7a011d761da2235f1
aLLeXUs <mrallexus@gmail.com>
2018-08-27 22:10:02 +0300

    Add search by current location

--- frontend/src/components/navbar/SearchCity.vue
+++ frontend/src/components/navbar/SearchCity.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="control">
-        <b-autocomplete
-            v-model.trim="findCity.query"
-            placeholder="Location"
-            :data="findCity.data"
-            :open-on-focus="true"
-            :loading="findCity.isFetching"
-            field="text"
-            class="navbar__search-autocomplete"
-            @input="loadCities"
-            @select="option => this.$emit('select', option)"
-        />
+        <b-field>
+            <b-autocomplete
+                v-model.trim="findCity.query"
+                placeholder="Location"
+                :data="findCity.data"
+                :open-on-focus="true"
+                :loading="findCity.isFetching"
+                field="text"
+                class="navbar__search-autocomplete"
+                @input="loadCities"
+                @select="option => this.$emit('select', option)"
+            />
+            <p class="control">
+                <button
+                    class="button location"
+                    @click="findByCurrentLocation"
+                >
+                    <b-icon pack="fas" icon="location-arrow"></b-icon>
+                </button>
+            </p>
+        </b-field>
     </div>
 </template>
 
@@ -27,6 +37,9 @@ export default {
                 data: [],
                 query: '',
                 isFetching: false
+            },
+            userLocation: {
+                center: []
             }
         };
     },
@@ -41,7 +54,28 @@ export default {
                 }, response => {
                     this.findCity.isFetching = false;
                 });
-        }, 250)
+        }, 250),
+        findByCurrentLocation() {
+            // this.findCity.query = 'Current location';
+            this.$emit('select', this.userLocation);
+        }
+    },
+    created() {
+        LocationService.getUserLocationData()
+            .then(coordinates => {
+                this.userLocation.center[0] = coordinates.lng;
+                this.userLocation.center[1] = coordinates.lat;
+            })
+            .catch(error => {
+                this.userLocation.center[0] = 30.5238;
+                this.userLocation.center[1] = 50.4547;
+            });
     }
 };
 </script>
+
+<style scoped>
+    .location {
+        color: #b8b8ba;
+    }
+</style>
