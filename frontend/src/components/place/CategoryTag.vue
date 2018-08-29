8b1393db3c1744cdb0f8f0364979c62d5bf68188
unknown <sandrokharchenko@gmail.com>
2018-08-29 16:37:00 +0300

    frontend: add components for category tags

---
+++ frontend/src/components/place/CategoryTag.vue
@@ -0,0 +1,52 @@
+<template>
+    <span 
+        class="tag is-primary is-rounded"
+        :class="{ active: isActive }"
+        @click="onClick"
+    >
+        {{ tag.name }}
+        <button v-if="isActive" class="delete is-small"></button>
+    </span>     
+</template>
+
+<script>
+export default {
+    name: 'CategoryTag',
+
+    props: {
+        tag: {
+            required: true,
+            type: Object,
+        },
+    },
+
+    data() {
+        return {
+            isActive: false
+        }
+    },
+
+    methods: {
+        onClick() {
+            this.isActive = !this.isActive;
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    @mixin darkenBackground {
+        background-color: darken(#7957D5, 10%) !important;
+    }
+    
+    .tag {
+        &:hover {
+            @include darkenBackground;
+            cursor: pointer;
+        }
+    }
+
+    .active {
+        @include darkenBackground;
+    }
+</style>
---
+++ frontend/src/components/place/CategoryTagsContainer.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="category-tags">
+        <span class="category-tags__title">Show only:</span>
+
+        <div class="tags">
+            <CategoryTag v-for="tag in tags" :key="tag.id" :tag="tag" />
+        </div>
+    </div>    
+</template>
+
+<script>
+import CategoryTag from './CategoryTag';
+
+export default {
+    name: 'CategoryTagsContainer',
+
+    components: {
+        CategoryTag,
+    },
+
+    props: {
+        tags: {
+            required: true,
+            type: Array,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .category-tags {
+        background: #f8f8f8;
+        border-top: 1px solid #fff;
+        border-bottom: 1px solid #e4e4e4;
+        padding: 5px 16px 8px 16px;
+
+        &__title {
+            color: #6e6e6e;
+            font-size: 11px;
+            float: left;
+            margin: 6px 5px 0 0;
+            text-shadow: 0 1px 0 #fff;
+            line-height: 100%;
+            font-weight: bold;
+        }
+    }
+</style>
