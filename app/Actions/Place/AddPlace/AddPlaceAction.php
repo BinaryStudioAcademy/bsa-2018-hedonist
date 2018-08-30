bed916dfc0165b6b39d1a0aaaaf1294b3753ad48
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-08-30 13:19:21 +0300

    Add transaction wrapper.

--- app/Actions/Place/AddPlace/AddPlaceAction.php
+++ app/Actions/Place/AddPlace/AddPlaceAction.php
@@ -7,6 +7,7 @@ use Hedonist\Entities\Place\Location;
 use Illuminate\Support\Facades\Storage;
 use Hedonist\Entities\Place\PlacePhoto;
 use Hedonist\Services\FileNameGenerator;
+use Hedonist\Services\TransactionServiceInterface;
 use Hedonist\Repositories\User\UserRepositoryInterface;
 use Hedonist\Repositories\City\CityRepositoryInterface;
 use Hedonist\Repositories\Place\PlaceRepositoryInterface;
@@ -22,6 +23,8 @@ class AddPlaceAction
     const FILE_STORAGE = 'upload/place';
     const DESCRIPTION_DEFAULT = 'Place owner\'s photo.';
 
+    private $transactionService;
+
     private $userRepository;
     private $cityRepository;
     private $placeRepository;
@@ -32,9 +35,11 @@ class AddPlaceAction
         UserRepositoryInterface $userRepository,
         CityRepositoryInterface $cityRepository,
         PlaceRepositoryInterface $placeRepository,
+        TransactionServiceInterface $transactionService,
         PlacePhotoRepositoryInterface $placePhotoRepository,
         PlaceCategoryRepositoryInterface $placeCategoryRepository
     ) {
+        $this->transactionService = $transactionService;
         $this->userRepository = $userRepository;
         $this->cityRepository = $cityRepository;
         $this->placeRepository = $placeRepository;
@@ -44,87 +49,90 @@ class AddPlaceAction
 
     public function execute(AddPlaceRequest $placeRequest): AddPlaceResponse
     {
-        $creator = $this->userRepository->getById($placeRequest->getCreatorId());
-        $category = $this->placeCategoryRepository->getById($placeRequest->getCategoryId());
+        return $this->transactionService->run(
+            function () use ($placeRequest) {
+                $creator = $this->userRepository->getById($placeRequest->getCreatorId());
+                $category = $this->placeCategoryRepository->getById($placeRequest->getCategoryId());
 
-        /* City - firstOrCreate */
-        $city = $this->cityRepository->findByNameAndLocation(
-            $placeRequest->getCity()['name'],
-            $placeRequest->getCity()['lng'],
-            $placeRequest->getCity()['lat']
-        );
+                /* City - firstOrCreate */
+                $city = $this->cityRepository->findByNameAndLocation(
+                    $placeRequest->getCity()['name'],
+                    $placeRequest->getCity()['lng'],
+                    $placeRequest->getCity()['lat']
+                );
 
-        try {
-            $location = new Location($placeRequest->getLongitude(), $placeRequest->getLatitude());
-        } catch (\InvalidArgumentException $e) {
-            throw new PlaceLocationInvalidException($e->getMessage());
-        }
+                try {
+                    $location = new Location($placeRequest->getLongitude(), $placeRequest->getLatitude());
+                } catch (\InvalidArgumentException $e) {
+                    throw new PlaceLocationInvalidException($e->getMessage());
+                }
 
-        if (!$creator) {
-            throw new PlaceCreatorDoesNotExistException;
-        }
+                if (!$creator) {
+                    throw new PlaceCreatorDoesNotExistException;
+                }
 
-        if (!$category) {
-            throw new PlaceCategoryDoesNotExistException;
-        }
+                if (!$category) {
+                    throw new PlaceCategoryDoesNotExistException;
+                }
 
-        /* Delete? with Exception, city CANNOT be null */
-        if (!$city) {
-            throw new PlaceCityDoesNotExistException;
-        }
+                /* Delete? with Exception, city CANNOT be null */
+                if (!$city) {
+                    throw new PlaceCityDoesNotExistException;
+                }
 
-        $place = $this->placeRepository->save(new Place([
-            'creator_id'  => $creator->id,
-            'category_id' => $category->id,
-            'city_id'     => $city->id,
-            'longitude'   => $location->getLongitude(),
-            'latitude'    => $location->getLatitude(),
-            'zip'         => $placeRequest->getZip(),
-            'address'     => $placeRequest->getAddress(),
-            'phone'       => $placeRequest->getPhone(),
-            'website'     => $placeRequest->getWebsite(),
-        ]));
-
-        /* PlaceLocalization */
-        $this->placeRepository->setTranslations($place, $placeRequest->getLocalization());
-
-        /* PlaceInfo */
-        $place->placeInfo()->create([
-            'place_id'     => $place->id,
-            'work_weekend' => $placeRequest->getWorkWeekend(),
-            'facebook'     => $placeRequest->getFacebook(),
-            'instagram'    => $placeRequest->getInstagram(),
-            'twitter'      => $placeRequest->getTwitter(),
-            'menu_url'     => $placeRequest->getMenuUrl()
-        ]);
-
-        /* PlaceTags */
-        $this->placeRepository->syncTags($place, $placeRequest->getTags());
-
-        /* PlaceFeatures */
-        $this->placeRepository->syncFeatures($place, $placeRequest->getFeatures());
-
-        /* PlacePhotos */
-        if ($photos = $placeRequest->getPhotos()) {
-            foreach ($photos as $photo) {
-                $fileNameGenerator = new FileNameGenerator($photo);
-                $fileName = $fileNameGenerator->generateFileName();
-                $path = Storage::putFileAs(self::FILE_STORAGE, $photo, $fileName, 'public');
-                list($width, $height) = getimagesize($photo);
-                $this->placePhotoRepository->save(new PlacePhoto([
-                    'creator_id' => $creator->id,
-                    'img_url' => $path,
-                    'description' => self::DESCRIPTION_DEFAULT,
-                    'place_id' => $place->id,
-                    'width' => $width,
-                    'height' => $height
+                $place = $this->placeRepository->save(new Place([
+                    'creator_id'  => $creator->id,
+                    'category_id' => $category->id,
+                    'city_id'     => $city->id,
+                    'longitude'   => $location->getLongitude(),
+                    'latitude'    => $location->getLatitude(),
+                    'zip'         => $placeRequest->getZip(),
+                    'address'     => $placeRequest->getAddress(),
+                    'phone'       => $placeRequest->getPhone(),
+                    'website'     => $placeRequest->getWebsite(),
                 ]));
-            }
-        }
 
-        /* PlaceWorktime */
-        $this->placeRepository->setWorktime($place, $placeRequest->getWorktime());
+                /* PlaceLocalization */
+                $this->placeRepository->setTranslations($place, $placeRequest->getLocalization());
 
-        return new AddPlaceResponse($place);
+                /* PlaceInfo */
+                $place->placeInfo()->create([
+                    'place_id'     => $place->id,
+                    'work_weekend' => $placeRequest->getWorkWeekend(),
+                    'facebook'     => $placeRequest->getFacebook(),
+                    'instagram'    => $placeRequest->getInstagram(),
+                    'twitter'      => $placeRequest->getTwitter(),
+                    'menu_url'     => $placeRequest->getMenuUrl()
+                ]);
+
+                /* PlaceTags */
+                $this->placeRepository->syncTags($place, $placeRequest->getTags());
+
+                /* PlaceFeatures */
+                $this->placeRepository->syncFeatures($place, $placeRequest->getFeatures());
+
+                /* PlacePhotos */
+                if ($photos = $placeRequest->getPhotos()) {
+                    foreach ($photos as $photo) {
+                        $fileNameGenerator = new FileNameGenerator($photo);
+                        $fileName = $fileNameGenerator->generateFileName();
+                        $path = Storage::putFileAs(self::FILE_STORAGE, $photo, $fileName, 'public');
+                        list($width, $height) = getimagesize($photo);
+                        $this->placePhotoRepository->save(new PlacePhoto([
+                            'creator_id' => $creator->id,
+                            'img_url' => $path,
+                            'description' => self::DESCRIPTION_DEFAULT,
+                            'place_id' => $place->id,
+                            'width' => $width,
+                            'height' => $height
+                        ]));
+                    }
+                }
+
+                /* PlaceWorktime */
+                $this->placeRepository->setWorktime($place, $placeRequest->getWorktime());
+
+                return new AddPlaceResponse($place);
+            });
     }
 }
