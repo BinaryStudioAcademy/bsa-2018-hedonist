2384a5e05cff1baa9225dea8e0948bb52bc2fd24
unknown <sandrokharchenko@gmail.com>
2018-08-31 21:33:42 +0300

    password-reset: add listener for reset password event

---
+++ app/Listeners/PasswordResetedEventListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Hedonist\Listeners;
+
+use Hedonist\Events\Auth\PasswordResetedEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+use Hedonist\Mail\ResetPasswordLinkSent;
+
+class PasswordResetedEventListener
+{   
+    public function handle(PasswordResetedEvent $event)
+    {
+        $user = $event->getUser();
+        $token = $event->getToken();
+
+        $message = (new ResetPasswordLinkSent($user, $token))
+            ->onConnection('beanstalkd');
+        
+        Mail::to($user)->queue($message);
+    }
+}
