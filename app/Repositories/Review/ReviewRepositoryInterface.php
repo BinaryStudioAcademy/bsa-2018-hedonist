064ec8e8184393e048b083c7276782c4919ab9db
MihailTS <tseluiko.m@gmail.com>
2018-09-02 15:20:05 +0300

    place_reviews: add methods for filtering to repository

--- app/Repositories/Review/ReviewRepositoryInterface.php
+++ app/Repositories/Review/ReviewRepositoryInterface.php
@@ -17,4 +17,10 @@ interface ReviewRepositoryInterface
     public function deleteById(int $id): void;
 
     public function findByCriteria(CriteriaInterface $criteria): Collection;
+
+    public function findCollectionByCriterias(CriteriaInterface ...$criterias): Collection;
+
+    public function setOrderBy(?string $column = 'id', ?string $direction = 'asc'): self;
+
+    public function getTotalCountByPlace(int $placeId): int;
 }
