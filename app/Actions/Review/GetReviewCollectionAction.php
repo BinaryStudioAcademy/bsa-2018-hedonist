539b6cb920ad49a805be47c3ffff5aede7e16a50
MihailTS <tseluiko.m@gmail.com>
2018-09-02 15:05:12 +0300

    place_reviews: add filters to GetReviewCollectionAction

--- app/Actions/Review/GetReviewCollectionAction.php
+++ app/Actions/Review/GetReviewCollectionAction.php
@@ -2,20 +2,56 @@
 
 namespace Hedonist\Actions\Review;
 
+use Hedonist\Actions\Presenters\Review\ReviewPresenter;
+use Hedonist\Repositories\Review\Criterias\GetReviewsByTextCriteria;
+use Hedonist\Repositories\Review\Criterias\GetReviewsByPlaceCriteria;
+use Hedonist\Repositories\Review\Criterias\ReviewPaginationCriteria;
 use Hedonist\Repositories\Review\ReviewRepositoryInterface;
 
 class GetReviewCollectionAction
 {
     private $reviewRepository;
+    private $reviewPresenter;
 
-    public function __construct(ReviewRepositoryInterface $repository)
+    public function __construct(ReviewRepositoryInterface $repository, ReviewPresenter $reviewPresenter)
     {
         $this->reviewRepository = $repository;
+        $this->reviewPresenter = $reviewPresenter;
     }
 
-    public function execute(): GetReviewCollectionResponse
+    public function execute(GetReviewCollectionRequest $request): GetReviewCollectionResponse
     {
-        $reviews = $this->reviewRepository->findAll();
-        return new GetReviewCollectionResponse($reviews);
+        $page = $request->getPage() ?? GetReviewCollectionRequest::DEFAULT_PAGE;
+        $placeId = $request->getPlaceId();
+        $defaultSort = 'created_at';
+        $defaultOrder = 'desc';
+        $sort = $request->getSort() ?? $defaultSort;
+        $order = $request->getOrder() ?? $defaultOrder;
+        $text = $request->getText();
+
+
+        $criterias = [];
+
+        if (!is_null($placeId)) {
+            $criterias[] = new GetReviewsByPlaceCriteria($placeId);
+        }
+
+        if (!is_null($text)) {
+            $criterias[] = new GetReviewsByTextCriteria($text);
+        }
+
+        $reviews = $this->reviewRepository
+            ->setOrderBy($sort, $order)
+            ->setOrderBy($defaultSort, $defaultOrder) //reviews needs multiply sort
+            ->findCollectionByCriterias(
+            new ReviewPaginationCriteria($page),
+            ...$criterias
+        )->map(function ($review) {
+            return $this->reviewPresenter->present($review);
+        });
+
+        $totalCount = $this->reviewRepository->getTotalCountByPlace($placeId);
+
+        return new GetReviewCollectionResponse($reviews, $totalCount, ReviewPaginationCriteria::LIMIT);
     }
 }
--- app/Actions/Review/GetReviewCollectionResponse.php
+++ app/Actions/Review/GetReviewCollectionResponse.php
@@ -2,19 +2,33 @@
 
 namespace Hedonist\Actions\Review;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 
 class GetReviewCollectionResponse
 {
     private $reviewCollection;
+    private $totalCount;
+    private $perPage;
 
-    public function __construct(Collection $reviews)
+    public function __construct(Collection $reviews, int $totalCount, int $perPage)
     {
         $this->reviewCollection = $reviews;
+        $this->totalCount = $totalCount;
+        $this->perPage = $perPage;
     }
 
     public function getReviewCollection(): array
     {
         return $this->reviewCollection->toArray();
+    }
+
+    public function getPaginationMetaInfo(): array
+    {
+        return [
+          'pagination' => [
+              'perPage' => $this->perPage,
+              'total' => $this->totalCount
+          ]
+        ];
     }
 }
