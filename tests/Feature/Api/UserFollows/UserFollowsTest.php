fd0096cbfd06600172d5b87b285145ffde05c180
Lesohorskyi Kirill <lesogor.kirill@gmail.com>
2018-09-04 18:11:48 +0300

    test shape

--- tests/Feature/Api/UserFollows/UserFollowsTest.php
+++ tests/Feature/Api/UserFollows/UserFollowsTest.php
@@ -3,9 +3,40 @@
 namespace Tests\Feature\Api\UserFollows;
 
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Feature\Api\ApiTestCase;
 
 class UserFollowsTest extends ApiTestCase
 {
+    use RefreshDatabase;
 
+    public function test_follow_user_success()
+    {
+
+    }
+
+    public function test_follow_invalid_user()
+    {
+
+    }
+
+    public function test_unfollow_user_success()
+    {
+
+    }
+
+    public function test_unfollow_invalid_user()
+    {
+
+    }
+
+    public function test_get_followers()
+    {
+
+    }
+
+    public function test_get_followed_users()
+    {
+
+    }
 }
