8dd6e1751dfb42d428aa3ea4e5a21569de916414
unknown <sandrokharchenko@gmail.com>
2018-09-04 16:32:25 +0300

    recovering-password: add queueing to mailer by default

--- app/Mail/ResetPasswordLinkSent.php
+++ app/Mail/ResetPasswordLinkSent.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ResetPasswordLinkSent extends Mailable
+class ResetPasswordLinkSent extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
