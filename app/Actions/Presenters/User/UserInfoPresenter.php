aa8be88077b10d0ed10c8862f0b9a982c7f73994
Lesohorskyi Kirill <lesogor.kirill@gmail.com>
2018-09-05 21:22:12 +0300

    style

--- app/Actions/Presenters/User/UserInfoPresenter.php
+++ app/Actions/Presenters/User/UserInfoPresenter.php
@@ -6,10 +6,10 @@ use Hedonist\Entities\User\UserInfo;
 
 class UserInfoPresenter
 {
-    public function present(UserInfo $info):array
+    public function present(UserInfo $info): array
     {
-        $result =  $info->toArray();
-        if($info->date_of_birth){
+        $result = $info->toArray();
+        if ($info->date_of_birth) {
             $result['date_of_birth'] = $info->date_of_birth->format('Y/m/d');
         }
         unset($result['id']);
