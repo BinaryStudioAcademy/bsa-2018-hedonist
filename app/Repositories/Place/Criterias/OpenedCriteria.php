d6228c7c493909b69181d3d0e880372fb34327e3
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-09-05 14:13:10 +0300

    Some changes.

--- app/Repositories/Place/Criterias/OpenedCriteria.php
+++ app/Repositories/Place/Criterias/OpenedCriteria.php
@@ -8,19 +8,14 @@ use Prettus\Repository\Contracts\RepositoryInterface;
 
 class OpenedCriteria implements CriteriaInterface
 {
-    private $today;
-
-    public function __construct()
-    {
-        $this->today = Carbon::now('UTC');
-    }
-
     public function apply($model, RepositoryInterface $repository)
     {
-        return $model->whereHas('worktime', function ($query) {
-            $query->where('day_code', strtolower(substr($this->today->format('l'), 0, 2)))
-                ->whereTime('start_time', '<=', $this->today->format('H:i'))
-                ->whereTime('end_time', '>=', $this->today->format('H:i'));
+        $today = Carbon::now('UTC');
+
+        return $model->whereHas('worktime', function ($query) use ($today) {
+            $query->where('day_code', strtolower(substr($today->format('l'), 0, 2)))
+                ->whereTime('start_time', '<=', $today->format('H:i'))
+                ->whereTime('end_time', '>=', $today->format('H:i'));
         });
     }
 }
