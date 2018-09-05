42e22f90e8d5d3f037fca65365e64035d9c52bc5
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2018-09-05 20:28:33 +0300

    change other users ui

--- frontend/src/components/users/ReviewsContainer.vue
+++ frontend/src/components/users/ReviewsContainer.vue
@@ -3,11 +3,6 @@
         <div class="user-reviews-container">
             <div class="user-reviews-header">
                 <h3 class="subtitle is-4">{{ userProfile.first_name }}'s recent reviews</h3>
-                <div class="header-btn">
-                    <a class="button is-info">
-                        <span>See all {{ filterAllReviewUser }} reviews</span>
-                    </a>
-                </div>
             </div>
             <ul class="columns is-variable is-4 is-multiline user-reviews-items">
                 <template v-for="place in filteredUsersPlaces">
@@ -42,10 +37,8 @@ export default {
         }),
         ...mapGetters('place', ['getReviewsById' , 'getUserReviewsAll']),
         filteredUsersPlaces: function () {
+            console.log(this.getReviewsById(parseInt(this.$route.params.id)));
             return this.getReviewsById(parseInt(this.$route.params.id));
-        },
-        filterAllReviewUser: function () {
-            return this.getUserReviewsAll(parseInt(this.$route.params.id)).length;
         },
     },
     created() {
