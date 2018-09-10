5cae163817853a706e5f4773c66eb8ee048b28da
Lesohorskyi Kirill <lesogor.kirill@gmail.com>
2018-09-10 12:07:13 +0300

    added removal button

--- frontend/src/components/navbar/SearchPlaceCategory.vue
+++ frontend/src/components/navbar/SearchPlaceCategory.vue
@@ -1,121 +1,139 @@
 <template>
     <div class="control">
-        <b-autocomplete
-            v-model.trim="findItems.query"
-            placeholder="I'm looking for..."
-            :data="findItems.data"
-            :open-on-focus="true"
-            :loading="findItems.isFetching"
-            class="navbar__search-autocomplete"
-            field="name"
-            @input="loadItems"
-            @select="option => this.$emit('select', option)"
-            ref="autocomplete"
-            @keyup.native.enter="$emit('keyup.native.enter')"
-        >
+        <b-field>
+            <b-autocomplete
+                    v-model.trim="findItems.query"
+                    placeholder="I'm looking for..."
+                    :data="findItems.data"
+                    :open-on-focus="true"
+                    :loading="findItems.isFetching"
+                    class="navbar__search-autocomplete"
+                    field="name"
+                    @input="loadItems"
+                    @select="option => this.$emit('select', option)"
+                    ref="autocomplete"
+                    @keyup.native.enter="$emit('keyup.native.enter')"
+            >
 
-            <template slot-scope="props">
-                <div class="search-block" v-if="props.option.place === undefined">
-                    <img :src="props.option.logo">
-                    <span>{{ props.option.name }}</span>
-                </div>
-                <router-link v-else :to="`/places/${props.option.id}`">
-                    <div class="search-block">
+                <template slot-scope="props">
+                    <div class="search-block" v-if="props.option.place === undefined">
                         <img :src="props.option.logo">
-                        <span>{{ props.option.nameForAutoComplete }}</span>
+                        <span>{{ props.option.name }}</span>
                     </div>
-                </router-link>
-            </template>
-        </b-autocomplete>
+                    <router-link v-else :to="`/places/${props.option.id}`">
+                        <div class="search-block">
+                            <img :src="props.option.logo">
+                            <span>{{ props.option.nameForAutoComplete }}</span>
+                        </div>
+                    </router-link>
+                </template>
+            </b-autocomplete>
+            <p class="control" v-if="showClearButton">
+                <button
+                        class="button"
+                        @click="clearInput"
+                >
+                    <b-icon pack="far" icon="times-circle" />
+                </button>
+            </p>
+        </b-field>
     </div>
 </template>
 
 <script>
 
-import {mapActions, mapGetters} from 'vuex';
-import _ from 'lodash';
+    import {mapActions, mapGetters} from 'vuex';
+    import _ from 'lodash';
 
-export default {
-    name: 'SearchPlaceCategory',
-    props: {
-        selectCity: {
-            type: Object,
-            required: true
-        }
-    },
-    data() {
-        return {
-            findItems: {
-                data: [],
-                query: '',
-                isFetching: false
-            },
-        };
-    },
-    methods: {
-        ...mapActions({
-            loadCategoriesByName: 'search/loadCategories',
-            loadPlaces: 'search/loadPlaces'
-        }),
-        loadItems: _.debounce(function () {
-            this.findItems.data = [];
-            this.findItems.isFetching = true;
-            if (this.findItems.query === '') {
-                this.loadCategoriesByName(this.findItems.query)
-                    .then( res => {
-                        this.findItems.data = res;
-                        this.findItems.isFetching = false;
-                    }, response => {
-                        this.findItems.isFetching = false;
-                    });
-            } else {
-                let polygon = '';
-                if (!_.isEmpty(this.selectCity)) {
-                    polygon = this.createPolygonByBBox(this.selectCity.bbox);
-                }
-                this.loadPlaces({name: this.findItems.query, polygon: polygon})
-                    .then( res => {
-                        let data = [];
-                        res.forEach(function (item, index) {
-                            data[index] = {
-                                id: item['id'],
-                                logo: item['photo']['img_url'],
-                                nameForAutoComplete: item['localization'][0]['name'] + ' - ' + item['city']['name'],
-                                name: item['localization'][0]['name'],
-                                place: true
-                            };
-                        });
-                        this.findItems.data = data;
-                        this.findItems.isFetching = false;
-                    }, response => {
-                        this.findItems.isFetching = false;
-                    });
+    export default {
+        name: 'SearchPlaceCategory',
+        props: {
+            selectCity: {
+                type: Object,
+                required: true
             }
-
-        }, 250),
-        init() {
-            this.loadCategoriesByName(this.findItems.query)
-                .then( res => {
-                    this.findItems.data = res;
-                });
         },
-        createPolygonByBBox(bbox) {
-            const x1 = bbox[0];
-            const y1 = bbox[1];
-            const x2 = bbox[2];
-            const y2 = bbox[3];
+        data() {
+            return {
+                findItems: {
+                    data: [],
+                    query: '',
+                    isFetching: false
+                },
+            };
+        },
+        methods: {
+            ...mapActions({
+                loadCategoriesByName: 'search/loadCategories',
+                loadPlaces: 'search/loadPlaces'
+            }),
+            clearInput(){
+                this.findItems.query = '';
+            },
+            loadItems: _.debounce(function () {
+                this.findItems.data = [];
+                this.findItems.isFetching = true;
+                if (this.findItems.query === '') {
+                    this.loadCategoriesByName(this.findItems.query)
+                        .then(res => {
+                            this.findItems.data = res;
+                            this.findItems.isFetching = false;
+                        }, response => {
+                            this.findItems.isFetching = false;
+                        });
+                } else {
+                    let polygon = '';
+                    if (!_.isEmpty(this.selectCity)) {
+                        polygon = this.createPolygonByBBox(this.selectCity.bbox);
+                    }
+                    this.loadPlaces({name: this.findItems.query, polygon: polygon})
+                        .then(res => {
+                            let data = [];
+                            res.forEach(function (item, index) {
+                                data[index] = {
+                                    id: item['id'],
+                                    logo: item['photo']['img_url'],
+                                    nameForAutoComplete: item['localization'][0]['name'] + ' - ' + item['city']['name'],
+                                    name: item['localization'][0]['name'],
+                                    place: true
+                                };
+                            });
+                            this.findItems.data = data;
+                            this.findItems.isFetching = false;
+                        }, response => {
+                            this.findItems.isFetching = false;
+                        });
+                }
 
-            return x1 + ',' + y1 + ';' + x2 + ',' + y1 + ';' + x2 + ',' + y2 + ';' + x1 + ',' + y2 + ';' + x1 + ',' + y1;
-        }
-    },
-    created() {
-        this.init();
-    }
-};
+            }, 250),
+            init() {
+                this.loadCategoriesByName(this.findItems.query)
+                    .then(res => {
+                        this.findItems.data = res;
+                    });
+            },
+            createPolygonByBBox(bbox) {
+                const x1 = bbox[0];
+                const y1 = bbox[1];
+                const x2 = bbox[2];
+                const y2 = bbox[3];
+
+                return x1 + ',' + y1 + ';' + x2 + ',' + y1 + ';' + x2 + ',' + y2 + ';' + x1 + ',' + y2 + ';' + x1 + ',' + y1;
+            }
+        },
+        created() {
+            this.init();
+        },
+        computed: {
+            showClearButton(){
+                return !!this.findItems.query;
+            }
+        },
+    };
 </script>
 
 <style>
-    .search-block{
+    .search-block {
         display: flex;
         align-items: center;
         cursor: pointer;
@@ -126,7 +144,7 @@ export default {
         padding-right: 3rem;
     }
 
-    .search-block img{
+    .search-block img {
         margin-right: 5px;
     }
 </style>
