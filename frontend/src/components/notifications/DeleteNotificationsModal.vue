800bae2ce9ae9702c766d8c577b74730e82c6c39
k0sen <caladien12@gmail.com>
2018-09-10 13:37:55 +0300

    feature/clear_notification_confirmation: centered text

--- frontend/src/components/notifications/DeleteNotificationsModal.vue
+++ frontend/src/components/notifications/DeleteNotificationsModal.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
+    <div class="notification-modal">
         <header class="modal-card-head">
             <p class="modal-card-title">{{ $t('notifications.confirm-deleting') }}</p>
         </header>
         <footer class="modal-card-foot">
-            <button class="button" type="button" @click="$parent.close()">
-                {{ $t('notifications.no') }}
-            </button>
-            <button class="button is-danger" @click="onDelete">
-                {{ $t('notifications.yes') }}
-            </button>
+            <div class="actions">
+                <button class="button" type="button" @click="$parent.close()">
+                    {{ $t('notifications.no') }}
+                </button>
+                <button class="button is-danger" @click="onDelete">
+                    {{ $t('notifications.yes') }}
+                </button>
+            </div>
         </footer>
     </div>
 </template>
@@ -27,6 +29,14 @@ export default {
 </script>
 
 <style scoped>
+    .notification-modal {
+        text-align: center;
+    }
+
+    .modal-card-foot > .actions {
+        margin: 0 auto;
+    }
+
     .button.is-danger:hover {
         background-color: #ce143a;
     }
