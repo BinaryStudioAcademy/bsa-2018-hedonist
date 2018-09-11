76339072aeb24d994034c756a87cd2fe1be0ed28
ivanshamis <ivanshamis@gmail.com>
2018-09-11 17:55:08 +0300

    place-page-ui-fixes: fix checkin button size

--- frontend/src/components/place/PlaceCheckin.vue
+++ frontend/src/components/place/PlaceCheckin.vue
@@ -63,10 +63,10 @@ export default {
     .checkin {
         border-radius: 7px;
         height: 48px;
-        font-size: 1.5rem;
+        font-size: 1.1rem;
         color: #FFF;
         text-align: center;
-        padding: 0 10px;
+        padding: 0 7px;
 
         i {
             padding-right: 5px;
