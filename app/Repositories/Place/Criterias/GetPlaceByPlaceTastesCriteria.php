7e60b41e4f2d9072dea4a12a55ee9f6c8e612974
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2018-09-11 13:02:50 +0300

    fix

--- app/Repositories/Place/Criterias/GetPlaceByPlaceTastesCriteria.php
+++ app/Repositories/Place/Criterias/GetPlaceByPlaceTastesCriteria.php
@@ -3,6 +3,8 @@
 namespace Hedonist\Repositories\Place\Criterias;
 
 use Hedonist\Entities\Place\Place;
+use Hedonist\Exceptions\DomainException;
+use Hedonist\Exceptions\Place\PlaceNotFoundException;
 use Prettus\Repository\Contracts\CriteriaInterface;
 use Prettus\Repository\Contracts\RepositoryInterface;
 use Illuminate\Database\Eloquent\Builder;
@@ -18,7 +20,7 @@ class GetPlaceByPlaceTastesCriteria implements CriteriaInterface
 
     public function apply($model, RepositoryInterface $repository)
     {
-        $placeTastes = $this->currentPlace->tastes->pluck('id');
+        $placeTastes = $this->currentPlace->tastes->pluck('id')->toArray();
         if (!empty($placeTastes)) {
             $model = $model->whereHas('tastes', function (Builder $query) use ($placeTastes) {
                 $query->whereIn('place_tastes.taste_id', $placeTastes);
