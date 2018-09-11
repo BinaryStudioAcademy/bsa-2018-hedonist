1447e2a4bc9fd57beca135163687d1daf20dab4a
Vladyslav Vereta <vereta.vlad@gmail.com>
2018-09-11 16:34:42 +0300

    Minor change.

--- frontend/src/store/modules/place/state.js
+++ frontend/src/store/modules/place/state.js
@@ -2,7 +2,7 @@ import { STATUS_NONE } from '@/services/api/codes';
 
 export default {
     places: [],
-    places_amount: null,
+    places_amount: 0,
     currentPlace: null,
     visitors: {
         byId: {},
