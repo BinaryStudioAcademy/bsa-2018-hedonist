c0bcef708ecf30a532b31fd067ffd2568656e252
pavlo <pavelnemoi@gmail.com>
2018-09-12 14:31:44 +0300

    Edit event review update listener, add elastic doc saving

--- app/Listeners/Review/ReviewAttitudeSetEventListener.php
+++ app/Listeners/Review/ReviewAttitudeSetEventListener.php
@@ -6,14 +6,26 @@ use Hedonist\Events\Review\{
     ReviewAttitudeSetEvent,
     ReviewAttitudeBroadcastEvent
 };
+use Hedonist\Repositories\Review\ReviewRepositoryInterface;
 
 class ReviewAttitudeSetEventListener
 {
+    private $reviewRepository;
+
+    public function __construct(ReviewRepositoryInterface $reviewRepository)
+    {
+        $this->reviewRepository = $reviewRepository;
+    }
+
     public function handle(ReviewAttitudeSetEvent $event)
     {
         broadcast(new ReviewAttitudeBroadcastEvent(
             $event->getReviewId(),
             $event->getAttitudeType()
         ))->toOthers();
+
+        $review = $this->reviewRepository->getById($event->getReviewId());
+
+        $review->document()->save();
     }
 }
