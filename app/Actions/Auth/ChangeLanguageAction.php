0d69975781bf7f736bd559d21e9911c8bb90ebb6
Alexander Kharchenko <sandrokharchenko@gmail.com>
2018-09-12 15:59:57 +0300

    remember-user-language: add action

---
+++ app/Actions/Auth/ChangeLanguageAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Hedonist\Actions\Auth;
+
+use Hedonist\Actions\Auth\Requests\ChangeLanguageRequest;
+use Hedonist\Entities\User\UserInfo;
+use Hedonist\Exceptions\Auth\InvalidLanguageException;
+use Hedonist\Repositories\User\UserInfoRepositoryInterface;
+
+class ChangeLanguageAction
+{
+    const ALLOWED_LANGUAGES = ['en', 'ua', 'ru'];
+    
+    private $repository;
+
+    public function __construct(UserInfoRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(ChangeLanguageRequest $request): void
+    {
+        $language = $request->getLanguage();
+        if (array_search($language, self::ALLOWED_LANGUAGES) === FALSE) {
+            throw new InvalidLanguageException();
+        }
+        $userInfo = $this->repository->getByUserId($request->getUserId());
+        $userInfo->language = $language;
+        $this->repository->save($userInfo);
+    }
+}
