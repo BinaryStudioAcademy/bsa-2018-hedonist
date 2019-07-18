01f5351bae7cfee6b2ef02c137ada6d97597d197
Pavel Nemchenko <pavelnemoi@gmail.com>
2019-07-18 20:18:36 +0000

    Apply fixes from StyleCI

--- database/migrations/2019_07_18_183518_user_soft_delete.php
+++ database/migrations/2019_07_18_183518_user_soft_delete.php
@@ -13,7 +13,7 @@ class UserSoftDelete extends Migration
      */
     public function up()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
@@ -25,7 +25,7 @@ class UserSoftDelete extends Migration
      */
     public function down()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropSoftDeletes();
         });
     }
